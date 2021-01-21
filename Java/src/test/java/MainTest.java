0fb18c65bde8dea8967ccddf5e45c85977816a38
Brandon Chan <brandon_chan@sfu.ca>
2021-01-20 18:49:11 -0800

    added test file directory structure, and test build works with gradlew.

---
+++ Java/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest{
+    @Test
+    public void testCase1(){
+        // nothing to test for now
+        //assertEquals("test","test");
+        System.out.println("passed");
+    }
+}
