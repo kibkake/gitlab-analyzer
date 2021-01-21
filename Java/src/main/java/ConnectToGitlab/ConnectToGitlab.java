c55b74b3cd81b6475928bc9a8527f06641ac5d75
arahilin <abtin_rahili_nejad@sfu.ca>
2021-01-20 17:49:49 -0800

    Added ability to pull data from gitlab

--- Java/src/main/java/ConnectToGitlab/ConnectToGitlab.java
+++ Java/src/main/java/ConnectToGitlab/ConnectToGitlab.java
@@ -3,11 +3,25 @@ package main.java.ConnectToGitlab;
 import org.gitlab.api.AuthMethod;
 import org.gitlab.api.GitlabAPI;
 import org.gitlab.api.TokenType;
+import org.gitlab.api.models.GitlabProject;
+import org.gitlab.api.models.GitlabUser;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ConnectToGitlab {
 
-    public static void connectGitlab(String token) {
+    public static void connectGitlab(String token) throws IOException {
         GitlabAPI api = GitlabAPI.connect("https://csil-git1.cs.surrey.sfu.ca", token, TokenType.ACCESS_TOKEN, AuthMethod.URL_PARAMETER);
+        GitlabUser user = api.getUser();
+        System.out.println("name: " + user.getName() + ".  email: " + user.getEmail());
+
+        List<GitlabProject> projects = api.getMembershipProjects();
+
+        for (int i = 0; i < projects.size(); i++) {
+            System.out.print(i + 1 + ". " + projects.get(i).getName());
+            System.out.println("  " + projects.get(i).getDefaultBranch());
+        }
 
     }
 }
--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main() {
         System.out.println("Hello");
 
-        ConnectToGitlab.connectGitlab("");//Create an access token on your gitlab account and pass it in. Remove when pushing to avoid extra conflicts
-
+        try {
+            ConnectToGitlab.connectGitlab("");//Create an access token on your gitlab account and pass it in. Remove when pushing to avoid extra conflicts
+        }catch(IOException exception){
+            System.out.println(exception.getMessage());
+        }
 
     }
 }
