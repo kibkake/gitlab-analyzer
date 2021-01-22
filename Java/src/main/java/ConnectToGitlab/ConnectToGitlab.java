9aaeb881e6d3762b3f07bb5c660109e67b0ecf37
arahilin <abtin_rahili_nejad@sfu.ca>
2021-01-22 14:13:17 -0800

    Added the ability to get changes and number of commits

--- Java/src/main/java/ConnectToGitlab/ConnectToGitlab.java
+++ Java/src/main/java/ConnectToGitlab/ConnectToGitlab.java
@@ -2,11 +2,9 @@ package main.java.ConnectToGitlab;
 
 import org.gitlab.api.AuthMethod;
 import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.Pagination;
 import org.gitlab.api.TokenType;
-import org.gitlab.api.models.GitlabCommit;
-import org.gitlab.api.models.GitlabMergeRequest;
-import org.gitlab.api.models.GitlabProject;
-import org.gitlab.api.models.GitlabUser;
+import org.gitlab.api.models.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,36 +18,30 @@ public class ConnectToGitlab {
 
         List<GitlabProject> projects = api.getMembershipProjects();
 
+        //List all projects
         for (int i = 0; i < projects.size(); i++) {
             System.out.print(i + 1 + ". " + projects.get(i).getName());
             System.out.println("  " + projects.get(i).getDefaultBranch());
         }
+        System.out.println();
 
-        System.out.println(api.getProject(25591).getName());
-        System.out.println(api.getProject(25591).getHttpUrl());
+        //Get name and url of newest project
+        System.out.println(api.getProject(projects.get(0).getId()).getName());
+        System.out.println(api.getProject(projects.get(0).getId()).getHttpUrl());
 
-
-        List<GitlabMergeRequest> mergeRequests = api.getMergedMergeRequests(api.getProject(25591));
+        //Get a list of merge requests
+        List<GitlabMergeRequest> mergeRequests = api.getMergeRequests(api.getProject(projects.get(0).getId()));
         for (int i = 0; i < mergeRequests.size(); i++) {
             System.out.print(i + 1 + ". " + mergeRequests.get(i).getTitle() + "   ");
         }
+        System.out.println("\n");
+
+        //Get the changes from latest merge request
+        System.out.println("Changes:  \n" + api.getMergeRequestChanges(projects.get(0).getId(), mergeRequests.get(0).getIid()).getChanges().get(0).getDiff().toUpperCase());
         System.out.println();
 
-        System.out.print("100. " + mergeRequests.get(4).getTitle() + "   ");
-        System.out.print("100. " + api.getCommits(api.getMergedMergeRequests(25591).get(4)).size());
-
-
-
-        List<GitlabCommit> gitlabCommits = api.getCommits(api.getMergedMergeRequests(25591).get(4));
-        for (int i = 0; i < gitlabCommits.size(); i++) {
-            System.out.print(i + 1 + ". " + gitlabCommits.get(i).getCommittedDate() + "   ");
-        }
-        System.out.println();
-        //System.out.println(hash.toString()gitlabCommits.get(0).hashCode());
-
-        //List<GitlabCommitDiff> gitlabCommitDiffs = api.getCommitDiffs(25591, Integer.toString(gitlabCommits.get(0).hashCode()));
-        //System.out.println(gitlabCommitDiffs.get(0).getDiff());
-
+        //Get the number of all commits from the project
+        System.out.print("Number of commits: " + api.getAllCommits(projects.get(0).getId()).size());
 
     }
 }
