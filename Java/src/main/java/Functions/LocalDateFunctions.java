df9b850e683aaea50438b74d182dc63bb21906a0
John Doknjas <jdoknjas@sfu.ca>
2021-01-25 22:39:15 -0800

    Checking if startDate > endDate

--- Java/src/main/java/Functions/LocalDateFunctions.java
+++ Java/src/main/java/Functions/LocalDateFunctions.java
@@ -5,9 +5,17 @@ import java.util.ArrayList;
 
 public class LocalDateFunctions {
 
+    // Continue here - JUnit test this function.
+
+    // Will return an ArrayList containing dates from startDate to endDate, inclusive.
+    // If startDate is greater than endDate, and empty ArrayList is returned.
     public static ArrayList<LocalDate> generateRangeOfDates(LocalDate startDate,
                                                      LocalDate endDate) {
-        ArrayList<LocalDate> rangeOfDates = new ArrayList<LocalDate>();
+        ArrayList<LocalDate> rangeOfDates = new ArrayList<>();
+
+        if (startDate.compareTo(endDate) > 0) {
+            return rangeOfDates;
+        }
 
         rangeOfDates.add(startDate);
 
