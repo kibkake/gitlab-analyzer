779bbf64985d3e100d6a05a32e4ce6dac1a200b7
John Doknjas <jdoknjas@sfu.ca>
2021-01-25 21:28:16 -0800

    A few DB skeleton functions

--- Java/src/main/java/DatabaseClasses/DatabaseFunctions.java
+++ Java/src/main/java/DatabaseClasses/DatabaseFunctions.java
@@ -2,5 +2,19 @@ package main.java.DatabaseClasses;
 
 public class DatabaseFunctions {
 
+    private boolean isUserAuthenticated() {
+        // This function gets the user to enter some kind of password, and checks
+        // somewhere in the DB that this password is correct. Then, they get access
+        // to their token (which is also stored in the DB).
 
+        return false; // Placeholder
+    }
+
+    private String retrieveUserToken(String userName) {
+        // Search the database for the token (a String?) matching userName.
+
+        return ""; // Placeholder
+    }
+
+    
 }
