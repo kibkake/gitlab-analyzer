51b5d3887dcc99e9f1e80e9b917299b900c95919
John Doknjas <jdoknjas@sfu.ca>
2021-01-28 12:06:29 -0800

    Improving the testLargeRangeOfDates test function

--- Java/src/test/java/Functions/LocalDateFunctionsTest.java
+++ Java/src/test/java/Functions/LocalDateFunctionsTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static main.java.Functions.LocalDateFunctions.generateRangeOfDates;
 import static org.junit.Assert.*;
@@ -31,20 +33,6 @@ public class LocalDateFunctionsTest {
         assertTrue(singleton.equals(generateRangeOfDates(start, end)));
     }
 
-    public boolean allDaysInOrder(ArrayList<LocalDate> list, boolean isLeapYear) {
-        int dayCounter = 1;
-
-        for (int i = 0; i < list.size(); i++)
-        {
-            if (list.get(i).getDayOfYear() != dayCounter) {
-                return false;
-            }
-            dayCounter++;
-        }
-
-        return true;
-    }
-
     @Test
     public void testLargeRangeOfDates() {
         LocalDate start = LocalDate.of(2010, 1, 1);
@@ -52,10 +40,49 @@ public class LocalDateFunctionsTest {
 
         ArrayList<LocalDate> list = generateRangeOfDates(start, end);
 
-        assertTrue(list.size() == 365);
+        assertTrue((list.size() == 365 && !start.isLeapYear()) ||
+                   (list.size() == 366 && start.isLeapYear()));
         assertTrue(list.get(0).getDayOfYear() == 1);
-        assertTrue(list.get(364).getDayOfYear() == 365);
+        assertTrue(list.get(list.size()-1).getDayOfYear() == list.size());
 
         assertTrue(allDaysInOrder(list, false));
+    }
+
+    // Helper functions for testing:
+
+    public boolean isFirstDayOfMonth(int dayNumber, boolean isLeapYear) {
+        if (dayNumber == 1 || dayNumber == 32) {
+            return true;
+        }
+
+        if (isLeapYear) {
+            dayNumber--;
+        }
+
+        return dayNumber == 60 || dayNumber == 91 || dayNumber == 121 ||
+                dayNumber == 152 || dayNumber == 182 || dayNumber == 213 ||
+                dayNumber == 244 || dayNumber == 274 || dayNumber == 305 ||
+                dayNumber == 335;
+    }
+
+    public boolean allDaysInOrder(ArrayList<LocalDate> list, boolean isLeapYear) {
+        int dayCounter = 0;
+        int monthCounter = 0;
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            dayCounter++;
+
+            if (isFirstDayOfMonth(dayCounter, isLeapYear)) {
+                monthCounter++;
+            }
+
+            if (list.get(i).getDayOfYear() != dayCounter ||
+                list.get(i).getMonthValue() != monthCounter) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
