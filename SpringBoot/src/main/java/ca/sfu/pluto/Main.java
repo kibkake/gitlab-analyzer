ba11c90f53d790a696d444b9113ec04be1de6f6c
Brandon Chan <brandon_chan@sfu.ca>
2021-01-27 22:16:40 -0800

    Added javadoc and bean debug printer
    The bean debug printer method was copied and reference from https://spring.io/guides/gs/spring-boot/. It only serves to debug beans created in the project.

--- SpringBoot/src/main/java/ca/sfu/pluto/Main.java
+++ SpringBoot/src/main/java/ca/sfu/pluto/Main.java
@@ -1,12 +1,44 @@
 package ca.sfu.pluto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.*;
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
 
+/**
+ * The main application that invokes SpringBoot's bootRun
+ */
 @SpringBootApplication
 public class Main {
 
+    /**
+     * This is the main method for running Spring Boot
+     *
+     * @param args arguments to the main method
+     */
     public static void main(String[] args){
         SpringApplication.run(Main.class,args);
+    }
+
+    /**
+     * The following method is provided from spring.io and only prints information about beans created by our application
+     *
+     * @see  <a href="https://spring.io/guides/gs/spring-boot/">Spring IO</a>
+     */
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
