a35d5d632d9a71d0f4a2e1f3382819589e62206c
Brandon Chan <brandon_chan@sfu.ca>
2021-01-27 23:10:39 -0800

    Added flexibility to run script for Gradle or Maven build.
    MS-DOS file allows for choice of Gradle or Maven. To exit from the application, you must Ctrl+C. This is intended since the Spring application runs on Apache or Tomcat webservers.

--- SpringBoot/run.bat
+++ SpringBoot/run.bat
@@ -1,2 +1,5 @@
 :: Runs the Spring Boot Application Server
-call gradle bootRun
+@ECHO OFF
+:: choose gradle or maven
+SET /p choice= "Gradle or Maven. Type g or m to indicate your choice..."
+IF "%choice%" == "g" ( CALL gradle bootRun) ELSE ( CALL mvn spring-boot:run)
