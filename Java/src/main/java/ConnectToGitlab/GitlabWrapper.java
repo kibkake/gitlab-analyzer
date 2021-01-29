443a31253fa85db4073bc2c05385ad8e460fc418
arahilin <abtin_rahili_nejad@sfu.ca>
2021-01-28 18:21:08 -0800

    Added method to get list of project from gitlab
    This method will retrieve a list of the user's(owenr of the token) project. This list only includes ones the user is a member of and is in json fromat.

--- Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
+++ Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
@@ -1,6 +1,8 @@
 package main.java.ConnectToGitlab;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -12,6 +14,20 @@ public class GitlabWrapper {
     public static HttpURLConnection makeConnection(URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         return connection;
+    }
+
+
+    public static void getListOfMembershipProjects(String token) throws IOException {
+        URL url = new URL(MAIN_URL + "?membership&" + "order_by=name&" + "simple=true" +"access_token=" + token);
+        HttpURLConnection connection = makeConnection(url);
+        connection.setRequestMethod("GET");
+        connection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String reply = "";
+        for (String oneLine; (oneLine = bufferedReader.readLine()) != null; reply += oneLine);
+        System.out.println(reply);
+        connection.disconnect();
 
     }
 
