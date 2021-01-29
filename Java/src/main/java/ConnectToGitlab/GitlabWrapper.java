b350b8ca1809a7cb5fe446add66e8c14740b8dbd
arahilin <abtin_rahili_nejad@sfu.ca>
2021-01-28 18:19:09 -0800

    Added method to connect to gitlab server
    This method will return a HttpURLConnection

--- Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
+++ Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
@@ -1,4 +1,18 @@
 package main.java.ConnectToGitlab;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class GitlabWrapper {
+
+    public static final String MAIN_URL = "https://cmpt373-1211-10.cmpt.sfu.ca/api/v4/projects";
+
+
+    public static HttpURLConnection makeConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        return connection;
+
+    }
+
 }
