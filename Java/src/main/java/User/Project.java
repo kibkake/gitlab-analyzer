1e1892326db2a0ec6f853511b7f6c11111ddc08e
nsimms <nsimms@sfu.ca>
2021-01-29 14:51:16 -0800

    "Add back missing main file"

--- Java/src/main/java/User/Project.java
+++ Java/src/main/java/User/Project.java
@@ -1,4 +1,36 @@
 package User;
 
-public class Repo {
+import java.time.LocalDate;
+import java.util.List;
+
+public class Project {
+    private int id;
+    private String name;
+    private String path;
+    private String path_with_namespace;
+    private LocalDate createdAt;
+    private LocalDate lastActivityAt;
+    private int openIssuesCount;
+    private int commitCount; // get from statistic label in JSON
+    private List<String> links; // URLS to: issues, merge_requests, repo_branches, labels, events and members
+
+    public Project() {
+    }
+
+    public Project(int id) {
+        this.id = id;
+    }
+
+    public Project(int id, String name, String path, String path_with_namespace, LocalDate createdAt,
+                   LocalDate lastActivityAt, int openIssuesCount, int commitCount, List<String> links) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+        this.path_with_namespace = path_with_namespace;
+        this.createdAt = createdAt;
+        this.lastActivityAt = lastActivityAt;
+        this.openIssuesCount = openIssuesCount;
+        this.commitCount = commitCount;
+        this.links = links;
+    }
 }
--- Java/src/main/java/User/User.java
+++ Java/src/main/java/User/User.java
@@ -1,2 +1,46 @@
-package User;public class User {
+package User;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String name;
+    private LocalDate lastSignInAt; // do not know if this is project specific
+
+    public User() {
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public User(int id, String name, LocalDate lastSignInAt) {
+        this.id = id;
+        this.name = name;
+        this.lastSignInAt = lastSignInAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getLastSignInAt() {
+        return lastSignInAt;
+    }
+
+    public void setLastSignInAt(LocalDate lastSignInAt) {
+        this.lastSignInAt = lastSignInAt;
+    }
 }
