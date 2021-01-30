e9f039a2d3f21bf7fc6d1973de3ccc50527be1c5
arahilin <abtin_rahili_nejad@sfu.ca>
2021-01-30 14:58:30 -0800

    Add ability to get issue iid
    The getAllProjectIssues method will now retrieve project's issues' iids and descriptions

--- Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
+++ Java/src/main/java/ConnectToGitlab/GitlabWrapper.java
@@ -132,15 +132,31 @@ public class GitlabWrapper {
     }
 
     public static void getAllProjectIssues(String token, int mergeIid) throws IOException {
-        //GET /projects/:id/issues
-        URL url = new URL(MAIN_URL + "/projects/" + mergeIid + "/issues?" + "access_token=" + token);
+        URL url = new URL(MAIN_URL + "/" + mergeIid + "/issues" + "?access_token=" + token);
         HttpURLConnection connection = makeConnection(url);
         connection.setRequestMethod("GET");
         connection.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String reply = "";
-        for (String oneLine; (oneLine = bufferedReader.readLine()) != null; reply += oneLine) ;
+        for (String oneLine; (oneLine = bufferedReader.readLine()) != null; reply += oneLine);
+
         //System.out.println(reply);
+        Gson gson = new Gson();
+        JsonArray jsonArray = gson.fromJson(reply, JsonArray.class);
+        List<Integer> issueIids = new ArrayList<>();
+        for(int i = 0; i< jsonArray.size(); i++) {
+            JsonElement jsonElement1 = jsonArray.get(i);
+            JsonObject jsonObject1 = jsonElement1.getAsJsonObject();
+            JsonPrimitive jsonPrimitiveIssueIid = jsonObject1.getAsJsonPrimitive("iid");
+            //System.out.println(jsonPrimitiveIssueIid.getAsInt());
+            issueIids.add(jsonPrimitiveIssueIid.getAsInt());
+            JsonPrimitive jsonPrimitiveIssueTitle = jsonObject1.getAsJsonPrimitive("title");
+            //System.out.println(jsonPrimitiveIssueTitle.getAsString());
+
+        }
+
+
+
     }
 
 
