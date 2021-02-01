3a2f0c06d90083faec60941b06fa0152b5c99f29
Eric Kwok <eka34@sfu.ca>
2021-02-01 10:07:26 -0800

    Implement method addUserToken
    addUserToken adds a document with a provided username and token.
    Document is added to "users" collection in the "gitlab" database

--- SpringBoot/src/main/java/ca/sfu/pluto/DatabaseFunctions.java
+++ SpringBoot/src/main/java/ca/sfu/pluto/DatabaseFunctions.java
@@ -1,6 +1,13 @@
 package ca.sfu.pluto;
 
-import main.java.Functions.LocalDateFunctions;
+// import main.java.Functions.LocalDateFunctions;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,6 +21,8 @@ import java.util.ArrayList;
 
 public class DatabaseFunctions {
 
+    private static String mongoDBURI = "mongodb+srv://Kae:mongopass@plutocluster.nop8i.mongodb.net/gitlab?retryWrites=true&w=majority";
+
     public static boolean isUserAuthenticated(/* args? */) {
         /* This function gets the user to enter some kind of password, and checks
            somewhere in the DB that this password is correct. Then, they get access
@@ -22,23 +31,41 @@ public class DatabaseFunctions {
         return false; // Placeholder
     }
 
-    public static String retrieveUserToken(String userName) {
-        // Search the database for the token (a String?) matching userName.
+    public static String retrieveUserToken(String username) {
+        // Search the database for the token (a String?) matching username.
 
         return ""; // Placeholder
     }
 
-    public static void addUserToken(String userName, String token) {
-        // Make a new row in some table of the DB, to store userName and token together.
+    /**
+     * Adds a document to the collection store a token for the given username
+     *
+     * @param username username to identify whose token is being stored
+     * @param token authentication token to be stored
+     */
+    public static void addUserToken(String username, String token) {
+        try (MongoClient mongoClient = MongoClients.create(mongoDBURI)) {
+
+            MongoDatabase gitlabDB = mongoClient.getDatabase("gitlab");
+            MongoCollection<Document> userCollection = gitlabDB.getCollection("users");
+
+            Document user = new Document("_id", new ObjectId());
+            user.append("username", username)
+                    .append("token", token);
+
+            userCollection.insertOne(user);
+
+            return;
+        }
     }
 
-    public static int numCommits(String userName, LocalDate startDate, LocalDate endDate) {
+    public static int numCommits(String username, LocalDate startDate, LocalDate endDate) {
         /* Find the number of commits made by the user, from the start date to
            the end date. */
 
         int numTotalCommits = 0;
 
-        ArrayList<LocalDate> datesToExamine = LocalDateFunctions.generateRangeOfDates(startDate, endDate);
+//        ArrayList<LocalDate> datesToExamine = LocalDateFunctions.generateRangeOfDates(startDate, endDate);
 
         /* Run through all dates in this list. For each of them, search the DB for that
            user and that date, adding the # commits to the sum? */
@@ -46,7 +73,7 @@ public class DatabaseFunctions {
         return numTotalCommits;
     }
 
-    public static void setNumCommits(String userName, LocalDate date, int numCommits) {
+    public static void setNumCommits(String username, LocalDate date, int numCommits) {
         /* In the DB, for the specified user on the given date, set the number of commits
            to numCommits.
 
@@ -54,18 +81,18 @@ public class DatabaseFunctions {
            decide what to do. */
     }
 
-    public static int numMergeRequests(String userName, LocalDate startDate, LocalDate endDate) {
+    public static int numMergeRequests(String username, LocalDate startDate, LocalDate endDate) {
         // Same idea as the numCommits() function, just for MRs.
 
         int numTotalMergeRequests = 0;
 
-        ArrayList<LocalDate> datesToExamine = LocalDateFunctions.generateRangeOfDates
-                                              (startDate, endDate);
+//        ArrayList<LocalDate> datesToExamine = LocalDateFunctions.generateRangeOfDates
+//                                              (startDate, endDate);
 
         return numTotalMergeRequests;
     }
 
-    public static void setNumMergeRequests(String userName, LocalDate date, int numMRs) {
+    public static void setNumMergeRequests(String username, LocalDate date, int numMRs) {
         // Similar idea to the setNumCommits() function.
     }
 }
