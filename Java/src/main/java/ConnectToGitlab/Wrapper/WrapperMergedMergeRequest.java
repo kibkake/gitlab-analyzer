3c0e55413b831ae02e8a66ca37ce7334460550f9
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-03 13:34:54 -0800

    Get commits of a project
    projects will have a list of all commits regardless of whether those commits are on merged merge requests or not

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperMergedMergeRequest.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperMergedMergeRequest.java
@@ -8,11 +8,20 @@ public class WrapperMergedMergeRequest {
     private int mergeRequestIid;
     private int gitlabProjectId;
     private String mergeRequestTitle;
+    private int mergeYear;
+    private int mergeMonth;
+    private int mergeDay;
     private List<WrapperCommit> mergeRequestCommits;
 
-
-
-
-
-
+    public WrapperMergedMergeRequest(int mergeRequestId, int mergeRequestIid, int gitlabProjectId, String mergeRequestTitle,
+                                     int mergeYear, int mergeMonth, int mergeDay) {
+        this.mergeRequestId = mergeRequestId;
+        this.mergeRequestIid = mergeRequestIid;
+        this.gitlabProjectId = gitlabProjectId;
+        this.mergeRequestTitle = mergeRequestTitle;
+        this.mergeYear = mergeYear;
+        this.mergeMonth = mergeMonth;
+        this.mergeDay = mergeDay;
+        //this.mergeRequestCommits = mergeRequestCommits;
+    }
 }
--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
@@ -7,7 +7,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class WrapperProject {
 
@@ -17,15 +23,28 @@ public class WrapperProject {
     private List<WrapperMergedMergeRequest> mergedMergeRequests;
     private List<WrapperCommit> allCommits;
 
-    public WrapperProject(String token, int gitlabProjectId, String gitlabProjectName, List<WrapperMergedMergeRequest> mergedMergeRequests, List<WrapperCommit> allCommits) throws IOException {
+    public WrapperProject(String token, int gitlabProjectId, String gitlabProjectName) throws IOException, ParseException {
         this.gitlabProjectId = gitlabProjectId;
         this.gitlabProjectName = gitlabProjectName;
-        //this.mergedMergeRequests = mergedMergeRequests;
         getMergedMergeRequests(token, gitlabProjectId);
-        this.allCommits = allCommits;
+        getAllCommitProjectCommits(token, gitlabProjectId);
     }
 
-    private void getMergedMergeRequests(String token, int projectId) throws IOException {
+    private static void getAllCommitProjectCommits(String token, int projectId) throws IOException {
+        URL url = new URL(MAIN_URL + "/" + projectId + "/repository/commits" +  "?access_token=" + token);
+        HttpURLConnection connection = makeConnection(url);
+        connection.setRequestMethod("GET");
+        connection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String reply = "";
+        for (String oneLine; (oneLine = bufferedReader.readLine()) != null; reply += oneLine) ;
+        //System.out.println(reply);
+        connection.disconnect();
+
+    }
+
+    private void getMergedMergeRequests(String token, int projectId) throws IOException, ParseException {
         URL url = new URL(MAIN_URL + "/" + projectId + "/merge_requests" + "?state=merged&" + "access_token=" + token);
         HttpURLConnection connection = makeConnection(url);
         connection.setRequestMethod("GET");
@@ -50,11 +69,32 @@ public class WrapperProject {
             int mergeRequestProjectId = jsonPrimitiveProjectId.getAsInt();
             JsonPrimitive jsonPrimitiveMergedAt = jsonObject.getAsJsonPrimitive("merged_at");
             String mergeRequestUntilDate = jsonPrimitiveMergedAt.getAsString();
+            JsonPrimitive jsonPrimitiveTitle = jsonObject.getAsJsonPrimitive("title");
+            String mergeRequestTitle = jsonPrimitiveMergedAt.getAsString();
+            int [] mergeDate = parsIsoDate(mergeRequestUntilDate);
 
-            //WrapperMergedMergeRequest mergeRequest = new WrapperMergedMergeRequest();
-            //mergedMergeRequests.add(mergeRequest);
-
+            WrapperMergedMergeRequest mergeRequest = new WrapperMergedMergeRequest(mergeRequestId,mergeRequestIid,
+                    mergeRequestProjectId, mergeRequestTitle, mergeDate[0], mergeDate[1], mergeDate[2]);
+            mergedMergeRequests.add(mergeRequest);
         }
+    }
+
+    public static int[] parsIsoDate(String isoDate) throws ParseException {
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'T'HH");
+        df1.setTimeZone(TimeZone.getTimeZone("PT"));
+        //Date result1 = df1.parse("2024-01-24T23:55:59.000+00:00");
+        Date result1 = df1.parse(isoDate);
+
+        //System.out.println(result1);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(result1);
+        int [] result = new int[3];
+
+        result[0] = cal.get(Calendar.YEAR);
+        result[1] = (cal.get(Calendar.MONTH)+1);
+        result[2] = cal.get(Calendar.DATE);
+        return result;
     }
 
     public static HttpURLConnection makeConnection(URL url) throws IOException {
