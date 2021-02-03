21a0340a72f61df85db54a3459077b6d0896293e
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-03 11:44:41 -0800

    Update project class
    The project class now has a constructor and getter/setter methods

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
@@ -4,11 +4,47 @@ import java.util.List;
 
 public class WrapperProject {
 
-    public int gitlabProjectId;
-    public String gitlabProjectName;
-    public List<WrapperMergedMergeRequest> mergedMergeRequests;
-    public List<WrapperCommit> commits;
+    private int gitlabProjectId;
+    private String gitlabProjectName;
+    private List<WrapperMergedMergeRequest> mergedMergeRequests;
+    private List<WrapperCommit> allCommits;
 
+    public WrapperProject(int gitlabProjectId, String gitlabProjectName, List<WrapperMergedMergeRequest> mergedMergeRequests, List<WrapperCommit> allCommits) {
+        this.gitlabProjectId = gitlabProjectId;
+        this.gitlabProjectName = gitlabProjectName;
+        this.mergedMergeRequests = mergedMergeRequests;
+        this.allCommits = allCommits;
+    }
 
+    public int getGitlabProjectId() {
+        return gitlabProjectId;
+    }
 
+    public String getGitlabProjectName() {
+        return gitlabProjectName;
+    }
+
+    public List<WrapperMergedMergeRequest> getMergedMergeRequests() {
+        return mergedMergeRequests;
+    }
+
+    public List<WrapperCommit> getAllCommits() {
+        return allCommits;
+    }
+
+    public void setGitlabProjectId(int gitlabProjectId) {
+        this.gitlabProjectId = gitlabProjectId;
+    }
+
+    public void setGitlabProjectName(String gitlabProjectName) {
+        this.gitlabProjectName = gitlabProjectName;
+    }
+
+    public void setMergedMergeRequests(List<WrapperMergedMergeRequest> mergedMergeRequests) {
+        this.mergedMergeRequests = mergedMergeRequests;
+    }
+
+    public void setAllCommits(List<WrapperCommit> allCommits) {
+        this.allCommits = allCommits;
+    }
 }
