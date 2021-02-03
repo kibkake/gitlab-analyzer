91264bc34d95e7c49ee973bcfb3cab82f4f65f72
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-03 12:57:06 -0800

    Add merged merge requests to project
    Project objects will have a list of merge request

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
@@ -1,19 +1,65 @@
 package main.java.ConnectToGitlab.Wrapper;
 
+import com.google.gson.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class WrapperProject {
 
+    public static final String MAIN_URL = "https://cmpt373-1211-10.cmpt.sfu.ca/api/v4/projects";
     private int gitlabProjectId;
     private String gitlabProjectName;
     private List<WrapperMergedMergeRequest> mergedMergeRequests;
     private List<WrapperCommit> allCommits;
 
-    public WrapperProject(int gitlabProjectId, String gitlabProjectName, List<WrapperMergedMergeRequest> mergedMergeRequests, List<WrapperCommit> allCommits) {
+    public WrapperProject(String token, int gitlabProjectId, String gitlabProjectName, List<WrapperMergedMergeRequest> mergedMergeRequests, List<WrapperCommit> allCommits) throws IOException {
         this.gitlabProjectId = gitlabProjectId;
         this.gitlabProjectName = gitlabProjectName;
-        this.mergedMergeRequests = mergedMergeRequests;
+        //this.mergedMergeRequests = mergedMergeRequests;
+        getMergedMergeRequests(token, gitlabProjectId);
         this.allCommits = allCommits;
+    }
+
+    private void getMergedMergeRequests(String token, int projectId) throws IOException {
+        URL url = new URL(MAIN_URL + "/" + projectId + "/merge_requests" + "?state=merged&" + "access_token=" + token);
+        HttpURLConnection connection = makeConnection(url);
+        connection.setRequestMethod("GET");
+        connection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String reply = "";
+        for (String oneLine; (oneLine = bufferedReader.readLine()) != null; reply += oneLine);
+        //System.out.println(reply);
+        connection.disconnect();
+
+        Gson gson = new Gson();
+        JsonArray jsonArray = gson.fromJson(reply, JsonArray.class);
+        for(int i = 0; i < jsonArray.size(); i++) {
+            JsonElement jsonElement = jsonArray.get(i);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            JsonPrimitive jsonPrimitiveId = jsonObject.getAsJsonPrimitive("id");
+            int mergeRequestId = jsonPrimitiveId.getAsInt();
+            JsonPrimitive jsonPrimitiveIid = jsonObject.getAsJsonPrimitive("iid");
+            int mergeRequestIid = jsonPrimitiveIid.getAsInt();
+            JsonPrimitive jsonPrimitiveProjectId = jsonObject.getAsJsonPrimitive("project_id");
+            int mergeRequestProjectId = jsonPrimitiveProjectId.getAsInt();
+            JsonPrimitive jsonPrimitiveMergedAt = jsonObject.getAsJsonPrimitive("merged_at");
+            String mergeRequestUntilDate = jsonPrimitiveMergedAt.getAsString();
+
+            //WrapperMergedMergeRequest mergeRequest = new WrapperMergedMergeRequest();
+            //mergedMergeRequests.add(mergeRequest);
+
+        }
+    }
+
+    public static HttpURLConnection makeConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        return connection;
     }
 
     public int getGitlabProjectId() {
