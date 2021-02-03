8317d16311c7c771a6bdea09b6edd3d67a2fc02f
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-03 12:03:52 -0800

    Update merge request class
    The merge request class will store id, name, and commits of a particular merge request

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperMergedMergeRequest.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperMergedMergeRequest.java
@@ -1,4 +1,18 @@
 package main.java.ConnectToGitlab.Wrapper;
 
+import java.util.List;
+
 public class WrapperMergedMergeRequest {
+
+    private int mergeRequestId;
+    private int mergeRequestIid;
+    private int gitlabProjectId;
+    private String mergeRequestTitle;
+    private List<WrapperCommit> mergeRequestCommits;
+
+
+
+
+
+
 }
