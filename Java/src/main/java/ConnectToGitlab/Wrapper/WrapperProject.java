860f979937369d17a2d29308b8790a2c36e9e48a
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-03 11:39:04 -0800

    Add classes for project information
    WrapperProject objects should be made from gitlab projects

---
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperProject.java
@@ -0,0 +1,14 @@
+package main.java.ConnectToGitlab.Wrapper;
+
+import java.util.List;
+
+public class WrapperProject {
+
+    public int gitlabProjectId;
+    public String gitlabProjectName;
+    public List<WrapperMergedMergeRequest> mergedMergeRequests;
+    public List<WrapperCommit> commits;
+
+
+
+}
