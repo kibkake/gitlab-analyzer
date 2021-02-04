140f0d985fb4426645d1a35b8878b550d97e2f7a
Brandon Chan <brandon_chan@sfu.ca>
2021-02-03 20:14:12 -0800

    Fixes #24 - the main folder is now /Java

--- Java/grunner.bat
+++ Java/grunner.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
 :: clean build
 CALL gradle clean
-:: run the unit tests
-@ECHO:
-ECHO "Running Tests..."
-CALL gradle test
 :: run the main application
 @ECHO:
 ECHO "Running Application: Main.class ..."
