94d4a7f0727ff7e73a7dd93f2df7c21c7a9067d8
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-05 11:52:25 -0800

    Add constructor and methods
    Added constructor and getter methods for the WrapperCommitDiff class

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperCommitDiff.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperCommitDiff.java
@@ -1,4 +1,44 @@
 package main.java.ConnectToGitlab.Wrapper;
 
 public class WrapperCommitDiff {
+
+    String newPath;
+    String oldPath;
+    boolean newFile;
+    boolean renamedFile;
+    boolean deletedFile;
+    String diff;
+
+    public WrapperCommitDiff(String newPath, String oldPath, boolean newFile, boolean renamedFile, boolean deletedFile, String diff) {
+        this.newPath = newPath;
+        this.oldPath = oldPath;
+        this.newFile = newFile;
+        this.renamedFile = renamedFile;
+        this.deletedFile = deletedFile;
+        this.diff = diff;
+    }
+
+    public String getNewPath() {
+        return newPath;
+    }
+
+    public String getOldPath() {
+        return oldPath;
+    }
+
+    public boolean isNewFile() {
+        return newFile;
+    }
+
+    public boolean isRenamedFile() {
+        return renamedFile;
+    }
+
+    public boolean isDeletedFile() {
+        return deletedFile;
+    }
+
+    public String getDiff() {
+        return diff;
+    }
 }
