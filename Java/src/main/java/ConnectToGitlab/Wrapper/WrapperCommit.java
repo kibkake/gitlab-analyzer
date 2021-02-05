5aad13be22ededc7f1da58359f08e7484cb34939
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-05 11:47:44 -0800

    Add commit diff class
    A commit diff class is added so commit object would have all the necessary information about the changes made

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperCommit.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperCommit.java
@@ -1,5 +1,8 @@
 package main.java.ConnectToGitlab.Wrapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WrapperCommit {
 
     private String id;
@@ -9,8 +12,7 @@ public class WrapperCommit {
     private int commitYear;
     private int commitMonth;
     private int commitDay;
-
-    //private List<commitdiffs>
+    private List<WrapperCommitDiff> wrapperCommitDiffs = new ArrayList<>();
 
 
     public WrapperCommit(String id, String authorName, String authorEmail, String title, int commitYear, int commitMonth, int commitDay) {
@@ -21,5 +23,37 @@ public class WrapperCommit {
         this.commitYear = commitYear;
         this.commitMonth = commitMonth;
         this.commitDay = commitDay;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCommitYear() {
+        return commitYear;
+    }
+
+    public int getCommitMonth() {
+        return commitMonth;
+    }
+
+    public int getCommitDay() {
+        return commitDay;
+    }
+
+    public List<WrapperCommitDiff> getWrapperCommitDiffs() {
+        return wrapperCommitDiffs;
     }
 }
---
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperCommitDiff.java
@@ -0,0 +1,4 @@
+package main.java.ConnectToGitlab.Wrapper;
+
+public class WrapperCommitDiff {
+}
