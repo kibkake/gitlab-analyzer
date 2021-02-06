844f61ef5884f7cdb54bada84075ab1a1e136286
yka87 <yeoran_kim@sfu.ca>
2021-02-05 17:05:27 -0800

    Fixed the class level comments as Javadoc style

--- Java/src/main/java/ScoreLogic/CodeContribution.java
+++ Java/src/main/java/ScoreLogic/CodeContribution.java
@@ -1,8 +1,9 @@
 package main.java.ScoreLogic;
 
-/*
-    This class manages the calculation of code contribution by number and frequency of commits and merge request.
-    For now, this class is written by assumption the required data is saved at DB.
+/**
+ * This class manages functions as to the calculation of code contribution
+ * by number and frequency of commits/merge request.
+ * For now, this class is written by assumption the required data is saved at DB.
  */
 
 public class CodeContribution {
--- Java/src/main/java/ScoreLogic/CommentContribution.java
+++ Java/src/main/java/ScoreLogic/CommentContribution.java
@@ -3,11 +3,11 @@ package main.java.ScoreLogic;
 import java.time.LocalDate;
 import java.util.List;
 
-/*
-    This class manages the calculation of comment contribution on code review/issues, by word count and frequency.
-
-    (**For now, this class is written by assumption the required data is saved at DB,
-     but the way to access to DB is not clear yet so consider it as dummy code.)
+/**
+ * This class manages functions as to the calculation of comment contribution on code review/issues,
+ * by word count and frequency.
+ * (For now, this class is written by assumption the required data is saved at DB,
+ * but the way to access to DB is not clear yet so consider it as dummy code.)
  */
 public class CommentContribution {
     /*
@@ -48,17 +48,14 @@ public class CommentContribution {
         this.numWordsOfCommentsOnMergeRequest = countWords(commentsOnMergeRequest);
     }
 
-
     public int countWords (List<String> comments) {
         if (comments == null || comments.isEmpty()) {
             return 0;
         }
 
-        String[] splitComments;
         int count = 0;
-
         for (String comment : comments) {
-            splitComments = comment.split("\\s+");
+            String[] splitComments = comment.split("\\s+");
             count =+ splitComments.length;
         }
 
@@ -70,4 +67,7 @@ public class CommentContribution {
         //save the data to the DB, there should be attributes of the two variables for each author
     }
 
+    public void saveFrequencyOfComments () {
+        //save the data to the DB, there should be an attribute of the variables for each author
+    }
 }
