a02c147c7b05dcfb9294d833a73818410c1af78c
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-07 10:00:27 -0800

    Add user class
    User class will use a project object to retrieve information about a certain user

--- Java/src/main/java/ConnectToGitlab/Wrapper/WrapperUser.java
+++ Java/src/main/java/ConnectToGitlab/Wrapper/WrapperUser.java
@@ -1,7 +1,18 @@
 package main.java.ConnectToGitlab.Wrapper;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class WrapperUser {
+
+    private final String NAME;
+    private final List<WrapperMergedMergeRequest> MERGED_MERGE_REQUESTS = new ArrayList<>();
+    private final List<WrapperIssue> ALL_ISSUES = new ArrayList<>();
+
+
+    public WrapperUser(String name, WrapperProject project) {
+        this.NAME = name;
+    }
 
 
 
