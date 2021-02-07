1e0813236a2bca454f3b7a547febd4cfe9f65294
Brandon Chan <brandon_chan@sfu.ca>
2021-02-06 20:16:14 -0800

    fixed Exception message bug

--- Java/src/main/java/Security/Authenticator.java
+++ Java/src/main/java/Security/Authenticator.java
@@ -5,30 +5,30 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.*;
 import javax.crypto.*;
+import javax.crypto.spec.*;
 
-final class Authenticator{
+public final class Authenticator{
     private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
     private static final byte[] CRYPTOSALT = ("gMsLe2L49faE23lS").getBytes(java.nio.charset.StandardCharsets.UTF_8);
     private static final int KEY_SIZE = 512;
     private static final int ITERATIONS = 100000;
 
     public static String encrypt(String password){
-        return this.secureEncrypt(password.toCharArray());
+        return secureEncrypt(password.toCharArray());
     }
 
-    private String secureEncrypt(char[] password){
+    private static String secureEncrypt(char[] password){
         PBEKeySpec keySpec = new PBEKeySpec(password,CRYPTOSALT,ITERATIONS,KEY_SIZE);
         try{
             return generateSecureKey(keySpec);
-        }catch(Throwable e){
-            System.err.println("Error in Authenticator.java: Printing Stack Trace:\n"+e.printStackTrace());
+        }catch(Throwable exception){
+            System.err.println("Error in Authenticator.java: \nMessage: "+exception.getMessage()+"\nPrinting Stack Trace:\n");
+            exception.printStackTrace();
             return null;
-        }finally {
-            keySpec.clearPassword();
         }
     }
 
-    private String generateSecureKey(PBEKeySpec keySpec)throws NoSuchAlgorithmException, InvalidKeySpecException{
+    private static String generateSecureKey(PBEKeySpec keySpec)throws NoSuchAlgorithmException, InvalidKeySpecException{
         SecretKeyFactory keyGen = SecretKeyFactory.getInstance(ALGORITHM);
         return Base64.getEncoder().encodeToString(keyGen.generateSecret(keySpec).getEncoded());
     }
