11a4840942076c9044226836aa780062ff0154d6
nsimms <nsimms@sfu.ca>
2021-02-11 12:04:25 -0800

    change controller to use the ProjectService

--- Java/src/main/java/DatabaseClasses/controller/ProjectController.java
+++ Java/src/main/java/DatabaseClasses/controller/ProjectController.java
@@ -1,8 +1,10 @@
 package main.java.DatabaseClasses.Controller;
 
+import main.java.ConnectToGitlab.Developer.Developer;
 import main.java.ConnectToGitlab.Project.Project;
 import main.java.ConnectToGitlab.Project.ProjectConnection;
 import main.java.DatabaseClasses.Repository.ProjectRepository;
+import main.java.DatabaseClasses.Service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +20,11 @@ import java.util.List;
 @RequestMapping("api/v1/")
 public class ProjectController {
 
-    @Autowired
-    private static ProjectRepository projectRepository;
+    private final ProjectService projectService;
 
-    public ProjectController(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
 
@@ -31,18 +33,23 @@ public class ProjectController {
     // Example of getting projects only with field of name
     @GetMapping("projects")
     public List<Project> getAllProjects() {
-        return ProjectConnection.getAllProjects();
-
+        if(projectService.getAllProjects().isEmpty()) {
+            List<Project> projects = ProjectConnection.getAllProjects();
+            projectService.saveNewProjects(projects);
+            return projects;
+        } else {
+            return projectService.getAllProjects();
+        }
     }
 
-
-    // Example of getting memberList by project id,
-    // But the current json object structure does not have the memberList inside Project structure
-    // so consider this just as an example pseudocode
     @GetMapping("projects/{projectId}")
-    @Query(value="{'project_ID': ?0}", fields="{'memberList' : 1, 'project_ID': 0}")
-    public List<Project> getAllMembersOfSingleProject(@RequestParam int projectId) {
-        return projectRepository.findByIdContaining(projectId);
+    public Project getProject(@PathVariable("projectId") int projectId) {
+        return projectService.getProject(projectId);
+    }
+
+    @GetMapping("projects/{projectId}/developers")
+    public List<Developer> getProjectDevelopers(@PathVariable("projectId") int projectId) {
+        return projectService.getProjectDevelopers(projectId);
     }
 
 }
