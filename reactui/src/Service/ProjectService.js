9214111546ebcd8467858a62000f9a20491505d1
yka87 <yeoran_kim@sfu.ca>
2021-02-12 21:44:25 -0800

    added blank service class for sending HTTP request

--- reactui/src/Service/ProjectService.js
+++ reactui/src/Service/ProjectService.js
@@ -6,7 +6,7 @@ const PROJECT_REST_API_URL = "http://localhost:8080/";
   This class contains methods for sending HTTP requests to Apis by spring.
  */
 
-// For now can't really work on... DB & RestController should be set
+// For now can't really work on... The structure of DB & RestController should be set in the backend
 class ProjectService {
 
     getAll() {
