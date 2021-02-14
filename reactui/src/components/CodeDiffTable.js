648bf9589b05d190abfd65fc4024b99577da73bd
yka87 <yeoran_kim@sfu.ca>
2021-02-14 15:00:18 -0800

    fixed code diff table

--- reactui/src/components/CodeDiffTable.js
+++ reactui/src/components/CodeDiffTable.js
@@ -1,58 +1,55 @@
 import "../App.css"
-import React,{ Component } from "react";
-import ProjectService from "../Service/ProjectService";
-import * as ReactBootStrap from "react-bootstrap"
+import React, { Component } from "react";
 import {Table} from 'react-bootstrap'
 
-class CodeDiffTable extends Component{
-
+// class CodeDiffTable extends Component{
+function CodeDiffTable() {
     // TODO: Do not delete, will be used once the API is set
-    constructor(props) {
-        super(props);
-        this.state = {
-            WrapperUser:[]
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         WrapperUser:[]
+    //     }
+    // }
+    //
+    // componentDidMount() {
+    //     ProjectService.getCodeDiff().then((response) => {
+    //         this.setState({WrapperUser: response.data})
+    //     });
+    // }
 
-    componentDidMount() {
-        ProjectService.getCodeDiff().then((response) => {
-            this.setState({WrapperUser: response.data})
-        });
-    }
+    //Fake data for table creation testing
+    const codeDiff = [
+        {mergeRequestId:3, date: 204, score: 2, commitComments: "initial commit", codeDiff: "Dsdfsdfsdf"},
+        {mergeRequestId:8, date: 304, score: 0, commitComments: "check out commit", codeDiff: "sdfsdfads"},
+    ];
 
-    // Fake data for table creation testing
-    // const codeDiff = [
-    //     {mergeRequestIid:3, date: 204, score: 2, commitComments: "initial commit", codeDiff: "Dsdfsdfsdf"},
-    //     {mergeRequestIid:8, date: 304, score: 0, commitComments: "check out commit", codeDiff: "sdfsdfads"},
-    // ]
+    return (
+        <div className="CodeDiffTable">
+            <Table striped bordered hover>
+                <tbody>
+                <tr>
+                    <td>MR ID</td>
+                    <td>Date</td>
+                    <td>Score</td>
+                    <td>Commit Comments</td>
+                    <td>Code Diff</td>
+                </tr>
+                {
+                    codeDiff.map((item, index)=>
+                        <tr key ={index}>
+                            <td>{item.mergeRequestId}</td>
+                            <td>{item.date}</td>
+                            <td>{item.score}</td>
+                            <td>{item.commitComments}</td>
+                            <td>{item.codeDiff}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
 
-    render () {
-        return (
-            <div className="CodeDiffTable">
-                <Table striped bordered hover>
-                    <tbody>
-                    <tr>
-                        <td>Date</td>
-                        <td>Score</td>
-                        <td>Commit Comments</td>
-                        <td>Code Diff</td>
-                    </tr>
-                    {
-                        this.state.WrapperUser.map(WrapperUser=>
-                            <tr>
-                                <td>{WrapperUser.mergeRequestIid}</td>
-                                <td>{WrapperUser.mergeRequestTitle}</td>
-                                {/*<td>{WrapperUser.score}</td>*/}
-                                {/*<td>{WrapperUser.commitComments}</td>*/}
-                                {/*<td>{WrapperUser.mergeRequestCommits.diff}</td>*/}
-                            </tr>
-                        )}
-                    </tbody>
-                </Table>
-
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default CodeDiffTable;
