0c276222d808db9604ef328ead8886d0d8b5cb98
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-15 18:34:57 -0800

    Recieve and parse commit json. Display numbers in chart

--- reactui/src/components/CommitChart.js
+++ reactui/src/components/CommitChart.js
@@ -4,21 +4,35 @@ import Button from "react-bootstrap/Button";
 
 class CommitChart extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             data: []
         };
     }
-    async componentDidMount() {
-        for(var arr=[],dt=new Date("2021-01-11"); dt<=new Date("2021-02-22"); dt.setDate(dt.getDate()+1)){
 
-            var dateStr = new Date(dt).toDateString();
-            var month = dateStr.split(" ")[1]
-            var day = dateStr.split(" ")[2]
-            var year = dateStr.split(" ")[3]
-            //http://localhost:8080/getuserstats/6/arahilin/2021/Jan/25
-            let url3 = 'http://localhost:8080/getuserstats/6/arahilin/' + year + '/' + month + '/' + day
+    async componentDidMount() {
+
+            var dateStr1 = new Date("1-11-2021").toLocaleDateString();
+            //month/day/2021
+            //2/12/2021
+            var day1 = dateStr1.split("/")[1];
+            var month1 = dateStr1.split("/")[0];
+            var year1 = dateStr1.split("/")[2];
+            var completeFromDate = month1 + "-" + day1 + "-" + year1
+
+            var dateStr2 = new Date("2-22-2021").toLocaleDateString();
+            //month/day/2021
+            //2/12/2021
+            var day2 = dateStr2.split("/")[1];
+            var month2 = dateStr2.split("/")[0];
+            var year2 = dateStr2.split("/")[2];
+            var completeToDate = month2 + "-" + day2 + "-" + year2
+            var arr=[];
+
+
+//http://localhost:8080/getuserstats/6/arahilin/1-11-2021/2-22-2021
+            let url3 = 'http://localhost:8080/getuserstats/6/arahilin/' + completeFromDate + "/" + completeToDate
             const result = await fetch(url3, {
                 method: 'GET',
                 headers: {
@@ -29,22 +43,77 @@ class CommitChart extends Component {
             const json = await result.json();
             var data = JSON.stringify(json);
             var DataArray = JSON.parse(data);
-            await DataArray.map(item => { arr.push(item)})
-            await DataArray.map(item => { console.log(item)})
+            await DataArray.map(item => {
+                arr.push(item)
+            })
+        console.log(arr)
 
-        }
-        this.setState({data:arr})
+
+
+       this.setState({data: arr})
     }
 
     render() {
-        return(
-        <div>
-            Hello
+
+        var getDaysArray = function(start, end) {
+            for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
+
+                arr.push(new Date(dt).toDateString());
+            }
+            return arr;
+        };
+
+        var d = '12/12/1955 12:00:00 AM';
+        d = d.split(' ')[0];
+
+        var comarr = this.state.data;
+        const daylist = getDaysArray(new Date("2021-01-11"),new Date("2021-02-22"));
+
+        //console.log(daylist);
+        return (
+            <div>
+                <HorizontalBar
+
+                    data={{labels: daylist,
+                        datasets: [
+                            {label: 'Number of Commits',
+                                data: comarr
+                            },
+                        ],
+
+                    }}
+                    width={500}
+                    height={daylist.length*10}
+
+
+                    options={
+                        {onClick: (e, elements) => {
+                                e.preventDefault();
+                                const chart = elements[0]._chart;
+                                const element = chart.getElementAtEvent(e)[0];
+                                const dataset = chart.data.datasets[element._datasetIndex];
+                                const xLabel = chart.data.labels[element._index];
+                                var month = xLabel.toString().split(" ")[1]
+                                var day = xLabel.toString().split(" ")[2]
+                                var year = xLabel.toString().split(" ")[3]
+
+                                window.location.href=  window.location.pathname + "/commits/" + day + "/" + month + "/" + year;
+
+                            } , maintainAspectRatio:true,
+                            scales: {
+                                xAxes:[
+                                    {
+                                        ticks:{
+                                            beginAtZero: true,
+                                        },
+                                    }
+                                ]
+                            }
+                        }}
+                />
             </div>
         )
     }
-
-
 }
 
 export default CommitChart
