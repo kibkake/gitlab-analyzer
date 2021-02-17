3037984dcc2e1c967af9661f968ccb88e65cfdea
kirbyf <kirby_fung@sfu.ca>
2021-02-16 19:09:46 -0800

    Connected 'Single Commit Diff' to backend

--- reactui/src/components/CodeDiffTable.js
+++ reactui/src/components/CodeDiffTable.js
@@ -2,9 +2,10 @@ import "../App.css"
 import React, { Component } from "react";
 import {Table} from 'react-bootstrap'
 import Highlight from 'react-highlight'
+import axios from "axios";
 
-// class CodeDiffTable extends Component{
-function CodeDiffTable() {
+class CodeDiffTable extends Component{
+// function CodeDiffTable() {
 
     // TODO: Do not delete, will be used once the API is set
     // constructor(props) {
@@ -21,37 +22,52 @@ function CodeDiffTable() {
     // }
 
     //Fake data for table creation testing
-    const codeDiff = [
-        {mergeRequestId:3, date: 204, score: 2, commitMsg: "initial commit", codeDiff: "@@ -5,7 +5,10 @@ public class Main {\n         // f1-1a\n         //f1-1a\n \n+        //f1-2a\n+\n+\n+        //f1-2a\n \n-        //f1-1a\n     }\n }\n"},
-        {mergeRequestId:8, date: 304, score: 0, commitMsg: "check out commit", codeDiff: "@@ -1,5 +1,9 @@\n public class NewClass {\n \n+    //f1-2a\n+    //f1-2a\n     //f1-1a\n     //f1-1a\n+    //f1-2a\n+    //f1-2a\n }\n"},
-    ];
+    // const codeDiff = [
+    //     {mergeRequestId:3, date: 204, score: 2, commitMsg: "initial commit", codeDiff: "@@ -5,7 +5,10 @@ public class Main {\n         // f1-1a\n         //f1-1a\n \n+        //f1-2a\n+\n+\n+        //f1-2a\n \n-        //f1-1a\n     }\n }\n"},
+    //     {mergeRequestId:8, date: 304, score: 0, commitMsg: "check out commit", codeDiff: "@@ -1,5 +1,9 @@\n public class NewClass {\n \n+    //f1-2a\n+    //f1-2a\n     //f1-1a\n     //f1-1a\n+    //f1-2a\n+    //f1-2a\n }\n"},
+    // ];
 
+    state={
+        commits:[]
+    }
 
-    return (
-        <div className="CodeDiffTable">
-            <Table striped bordered hover>
-                <tbody>
-                <tr>
-                    <td>MR ID</td>
-                    <td>Date</td>
-                    <td>Score</td>
-                    <td>Commit Message</td>
-                    <td>Code Diff</td>
-                </tr>
-                {
-                    codeDiff.map((item, index)=>
-                        <tr key ={index}>
-                            <td>{item.mergeRequestId}</td>
-                            <td>{item.date}</td>
-                            <td>{item.score}</td>
-                            <td>{item.commitMsg}</td>
-                            <td><Highlight className="highlighted-text">{item.codeDiff}</Highlight></td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
-        </div>
-    );
+    componentDidMount(){
+        var str = window.location.pathname;
+        var repNum = str.split("/")[2];
+        var name = str.split("/")[4];
+        axios.get("http://localhost:8080/api/v1/projects/allCommits/"+repNum+"/"+name)
+        .then(response=>{
+            const commits = response.data
+            this.setState({commits})
+        })
+    }
+    render(){
+        return (
+            <div className="CodeDiffTable">
+                <Table striped bordered hover>
+                    <tbody>
+                    <tr>
+                        <td>Commit ID</td>
+                        <td>Date</td>
+                        <td>Score</td>
+                        <td>Commit Message</td>
+                        <td>Code Diff</td>
+                    </tr>
+                    {
+                        this.state.commits.map((item, index)=>
+                            <tr key ={index}>
+                                <td>{item.id}</td>
+                                <td>{item.date}</td>
+                                <td>{item.commitScore}</td>
+                                <td>{item.message}</td>
+                                <td><Highlight className="highlighted-text">{item.codeDiff}</Highlight></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
 
 export default CodeDiffTable;
