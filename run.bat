50f40184614111e0be4f07c9258a2e8e53e35d06
Brandon Chan <brandon_chan@sfu.ca>
2021-02-16 17:41:56 -0800

    revert multimodule gradle build to JAR.

--- run.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-:: invoke gradle fresh build on Java
-CALL Java/build
-:: invoke JAR production build
-CALL gradle clean build
-:: execute JAR
-CALL java -jar Java/build/libs/Java-0.0.2-SNAPSHOT.jar
