c10ba6189b3e3fc227c33c56dae9fd76a5a6343e
yka87 <yeoran_kim@sfu.ca>
2021-02-21 17:07:47 -0800

    set commentScore chart x-axis

--- reactui/src/components/CommentChart.js
+++ reactui/src/components/CommentChart.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import BarChart from 'react-bar-chart';
+import {BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 import axios from "axios";
+import * as d3 from "d3-time";
+import moment from "moment";
 
 const margin = {top: 20, right: 20, bottom: 30, left: 40};
 
-//[https://github.com/MateusZitelli/react-bar-chart]
+
 class CommentChart extends Component {
 
     getInitialState() {
@@ -48,55 +50,54 @@ class CommentChart extends Component {
             };
         });
         console.log(output);
+        const from = Number(new Date('2021-01-01'));
+        const to = Number(new Date('2021-02-23'));
 
         return (
-            <div ref='root'>
-                <div style={{width: '95%'}}>
-                    <BarChart ylabel='Score'
-                              width={1500}
-                              height={200}
-                              margin={margin}
-                              data={output}
-                              color="#ffe0b2"
-                              onBarClick={this.handleBarClick}/>
-
-                </div>
+            <div>
+                <ResponsiveContainer width = '100%' height = {500} >
+                    <BarChart
+                        data={output}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey= "date"
+                               type ="number"
+                               name = 'date'
+                               domain={[
+                                   d3.timeDay.floor(from).getTime(),
+                                   d3.timeDay.ceil(to).getTime()
+                               ]}
+                               tickFormatter = {(unixTime) => moment(unixTime).format('YYYY-MM-DD')}
+                        />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="wordCount" stackId="a" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         );
     }
+        // return (
+        //     <div ref='root'>
+        //         <div style={{width: '95%'}}>
+        //             <BarChart ylabel='Score'
+        //                       width={1500}
+        //                       height={200}
+        //                       margin={margin}
+        //                       data={output}
+        //                       color="#ffe0b2"
+        //                       onBarClick={this.handleBarClick}/>
+        //
+        //         </div>
+        //     </div>
+        // );
 }
-//
-// // creating a stacked barchart for mr/commits
-// const CommentCharts = () => {
-//     return <div>
-//         <Bar data={{
-//             //labels x for the axis
-//             labels: '',
-//             //data displayed in graph
-//             datasets: [
-//                 {
-//                     label: 'Comment Score',
-//                     data: [56, 34, 5, 50, 12],
-//                     backgroundColor:'lightGreen',
-//                     borderColor:'black'
-//                 },
-//             ]
-//         }}
-//              height={200}
-//              width={200}
-//              options ={{
-//                  maintainAspectRatio: false,
-//                  scales:{
-//                      yAxes:[
-//                          {
-//                              ticks:{
-//                                  beginAtZero: true,
-//                              }
-//                          }
-//                      ]
-//                  }
-//              }}
-//         />
-//     </div>
-// }
+
 export default CommentChart
