0a176a004cba08f74f607ca9effbb25c8e8dfb43
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-25 14:59:50 -0800

    Add initial value

--- reactui/src/components/DropDownMenu.js
+++ reactui/src/components/DropDownMenu.js
@@ -3,19 +3,26 @@ import {useState} from 'react'
 import Select from 'react-select'
 
 
-function DropDownMenu ({listOfDevelopers}) {
+function DropDownMenu ({listOfDevelopers, sentDev}) {
 
+    const arr = [];
+    listOfDevelopers.map(item => {arr.push({label: item, value: item})})
+
+    const pathArray = window.location.pathname.split('/');
+    const developer = pathArray[4];
 
     const[selectedValue, setSelectedValue] = useState(
-        null
+        developer
     );
     console.log("list of devs")
     console.log(listOfDevelopers)
 
     return (
         <div>
-            <div className="Footer">
+            <div>
                 <Select
+                    options={arr}
+                    defaultValue={{ label: developer, value: developer }}
                 />
             </div>
         </div>
