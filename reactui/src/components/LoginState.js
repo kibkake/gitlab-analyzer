9ccb3c2b4a2a18a0739adf1dc1b501f1473eaf65
Brandon Chan <brandon_chan@sfu.ca>
2021-02-25 20:01:51 -0800

    redirects to date time range.

--- reactui/src/components/LoginState.js
+++ reactui/src/components/LoginState.js
@@ -17,7 +17,7 @@ function LoginState({ setUser }) {
         LoginService.checkUserCredentials(username,password).then((response) => {
             if(response.data === true){
                 setUser(username);
-                window.location.reload();
+                window.location.href("/Settings");
             }else{
                 alert('Login Failed: Credentials Invalid!');
             }
