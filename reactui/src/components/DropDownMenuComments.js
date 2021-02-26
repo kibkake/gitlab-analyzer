631657243a19f2f3a41e4d0e8e717d22d934f0a3
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-26 13:14:59 -0800

    Add dropdown menu for the comments page

---
+++ reactui/src/components/DropDownMenuComments.js
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import Select from 'react-select'
+import Navbar_Developers from "./Navbar_Developers";
+import CustomizedRadios from "./RadioButton";
+import CommentTable from "./CommentTable";
+
+function DropDownMenuComments ({listOfDevelopers, sentDev}) {
+
+    const devArray = [];
+    listOfDevelopers.map(item => {
+        devArray.push({label: item, value: item})
+    })
+
+    const currentDeveloper = sessionStorage.getItem("CurrentDeveloper")
+    const [selectedValue, setSelectedValue] = useState(
+        null
+    );
+
+    const handleChange = obj => {
+        setSelectedValue(obj.label);
+        sessionStorage.setItem("CurrentDeveloper", obj.label);
+    }
+
+    return (
+
+        <div>
+            <Navbar_Developers devName = {sessionStorage.getItem("CurrentDeveloper")}/>
+            <br>
+            </br>
+            <Select
+                options={devArray}
+                defaultValue={{ label: currentDeveloper, value: currentDeveloper }}
+                onChange={handleChange}/>
+            <br>
+            </br>
+            <h1 style={{textAlign:'center'}}>Comment Contribution</h1>
+            <br></br>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <CustomizedRadios/>
+            </div>
+            <br></br>
+            <h4 style={{textAlign:'center'}}>Top 10 comments </h4>
+            <CommentTable devName = {sessionStorage.getItem("CurrentDeveloper")}/>
+
+        </div>
+    )
+}
+
+export default DropDownMenuComments;
