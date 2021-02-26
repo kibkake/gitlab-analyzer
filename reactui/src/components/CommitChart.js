7202a0d548cd51f342b83714d700006d744c51c3
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-25 22:36:08 -0800

    refactor CommitChart

--- reactui/src/components/CommitChart.js
+++ reactui/src/components/CommitChart.js
@@ -16,113 +16,14 @@ class CommitChart extends Component {
     }
 
     async componentDidMount() {
-
-        var arr2=[];
         const {parentdata} = this.state;
-
-
-        const result2 = await fetch('/api/v1/getstartdate', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        const json2 = await result2.json();
-        var data2 = JSON.stringify(json2);
-        var DataArray2 = JSON.parse(data2);
-
-        await DataArray2.map(item => {
-            arr2.push(item)
-        })
-
-        this.setState({strDate: arr2[0]})
-
-        var arr3=[];
-
-        const result3 = await fetch('/api/v1/getenddate', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        const json3 = await result3.json();
-        var data3 = JSON.stringify(json3);
-        var DataArray3 = JSON.parse(data3);
-
-        await DataArray3.map(item => {
-            arr3.push(item)
-        })
-
-        this.setState({endDate: arr3[0]})
-
-        var str = window.location.pathname;
-            var projNum = str.split("/")[2];
-            var developerName = str.split("/")[4];
-
-            var dateStr1 = new Date(this.state.strDate).toLocaleDateString();
-
-            var day1temp = dateStr1.split("/")[1];
-            var month1temp = dateStr1.split("/")[0];
-            var year1 = dateStr1.split("/")[2];
-            var day1;
-            var month1;
-            if(day1temp < 10){
-                day1 = '0' + day1temp;
-            }else{
-                day1 = day1temp;
-            }
-            if(month1temp < 10){
-                month1 = '0' + month1temp;
-            }else{
-                month1 = month1temp;
-            }
-
-            var completeFromDate = year1 + "-" + month1 + "-" + day1;
-
-            var dateStr2 = new Date(this.state.endDate).toLocaleDateString();
-            var day2temp = dateStr2.split("/")[1];
-            var month2temp = dateStr2.split("/")[0];
-            var year2 = dateStr2.split("/")[2];
-            var day2;
-            var month2;
-            if(day2temp < 10){
-                day2 = '0' + day2temp;
-            }else{
-                day2 = day2temp;
-            }
-            if(month2temp < 10){
-                month2 = '0' + month2temp;
-            }else{
-                month2 = month2temp;
-            }
-
-            var completeToDate = year2 + "-" + month2 + "-" + day2;
-            var arr=[];
-
-            let url3 = '/api/v1/projects/' + projNum + '/Commitsarray/' + parentdata + '/' +
-                completeFromDate + "/" + completeToDate
-            const result = await fetch(url3, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            })
-            const json = await result.json();
-            var data = JSON.stringify(json);
-            var DataArray = JSON.parse(data);
-            await DataArray.map(item => {
-                arr.push(item)
-            })
-       this.setState({data: arr})
+        this.getDataFromBackend(parentdata)
     }
 
-    async getMoreData(dev){
+    async getDataFromBackend (username) {
+
         var arr2=[];
         const {parentdata} = this.state;
-
 
         const result2 = await fetch('/api/v1/getstartdate', {
             method: 'GET',
@@ -204,7 +105,7 @@ class CommitChart extends Component {
         var completeToDate = year2 + "-" + month2 + "-" + day2;
         var arr=[];
 
-        let url3 = '/api/v1/projects/' + projNum + '/Commitsarray/' + dev + '/' +
+        let url3 = '/api/v1/projects/' + projNum + '/Commitsarray/' + username + '/' +
             completeFromDate + "/" + completeToDate
         const result = await fetch(url3, {
             method: 'GET',
@@ -225,7 +126,7 @@ class CommitChart extends Component {
     componentDidUpdate(prevProps){
         if(this.props.devName !== prevProps.devName){
             this.setState({parentdata: this.props.devName});
-            this.getMoreData(this.props.devName)
+            this.getDataFromBackend(this.props.devName)
         }
     }
 
