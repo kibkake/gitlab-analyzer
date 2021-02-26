ad58554f8980f8da076e882b6a1ab91beaa2dbb8
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-26 12:30:09 -0800

    Add dropdown menu for the MergeRequest page

---
+++ reactui/src/components/DropDownMenuMerge.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import {useState} from 'react'
+import Select from 'react-select'
+import Navbar_Developers from "./Navbar_Developers";
+import CommitChart from "./CommitChart";
+import CodeDiffTable from "./CodeDiffTable";
+
+
+function DropDownMenuMerge ({listOfDevelopers, sentDev}) {
+
+    const devArray = [];
+    listOfDevelopers.map(item => {
+        devArray.push({label: item, value: item})
+    })
+
+    const currentDeveloper = sessionStorage.getItem("CurrentDeveloper")
+    const [selectedValue, setSelectedValue] = useState(
+        null
+    );
+
+    const handleChange = obj => {
+        setSelectedValue(obj.label);
+        sessionStorage.setItem("CurrentDeveloper", obj.label);
+    }
+
+    return (
+
+        <div classname='CodeDiff'>
+            <Navbar_Developers devName = {sessionStorage.getItem("CurrentDeveloper")}/>
+            <br>
+            </br>
+            <Select
+                options={devArray}
+                defaultValue={{ label: currentDeveloper, value: currentDeveloper }}
+                onChange={handleChange}/>
+            <br>
+            </br>
+            <h1 style={{textAlign: 'center'}}>List of MR + full diff</h1>
+            <h2 style={{textAlign: 'center'}}>-list of commits for each MR + code diff</h2>
+            <h2 style={{textAlign: 'center'}}>-code diff should be shown in the same page</h2>
+            <h4 style={{textAlign: 'center'}}>-Highlight + part?</h4>
+
+            <CodeDiffTable  devName = {sessionStorage.getItem("CurrentDeveloper")}/>
+
+        </div>
+    )
+
+
+}
+
+export default DropDownMenuMerge;
