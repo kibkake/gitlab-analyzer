021cbc48884d47c2dc49408e869fdffa99724f85
arahilin <abtin_rahili_nejad@sfu.ca>
2021-02-25 19:12:38 -0800

    Add dropmenu for commit page

---
+++ reactui/src/components/DropDownMenuCommits.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import {useState} from 'react'
+import Select from 'react-select'
+import SummaryScoreTable from "./SummaryScoreTable";
+import StackedBarChart from "./StackedBarChart";
+import CommentChart from "./CommentChart";
+import Navbar_Developers from "./Navbar_Developers";
+import CommitChart from "./CommitChart";
+
+
+function DropDownMenuCommit ({listOfDevelopers, sentDev}) {
+
+    const devArray = [];
+    listOfDevelopers.map(item => {
+        devArray.push({label: item, value: item})
+    })
+
+    const pathArray = window.location.pathname.split('/');
+    const developer = pathArray[4];
+
+    const [selectedValue, setSelectedValue] = useState(
+        developer
+    );
+
+    const handleChange = obj => {
+        setSelectedValue(obj.label);
+    }
+
+    return (
+
+        <div>
+            <Navbar_Developers/>
+            <br>
+            </br>
+            <Select
+                options={devArray}
+                defaultValue={{ label: developer, value: developer }}
+                onChange={handleChange}/>
+            <br>
+            </br>
+            <CommitChart  devName = {selectedValue}/>
+
+        </div>
+    )
+
+
+}
+
+export default DropDownMenuCommit;
