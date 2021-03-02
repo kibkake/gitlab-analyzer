7b9ae772ce9bf62e4a99e091e1d70d2de2fbe5ca
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-02 12:45:23 -0800

    add handlers

--- reactui/src/components/DateRangeSummary.js
+++ reactui/src/components/DateRangeSummary.js
@@ -1,3 +1,5 @@
+import React,{ Component } from "react";
+import ProjectService from "../Service/ProjectService";
 
 function DateRangeSummary(){
 
@@ -10,7 +12,8 @@ function DateRangeSummary(){
             return new Date (localStorage.getItem("startdate"))
         }
         else {
-            return new Date ("2021-01-11")
+            sessionStorage.setItem("startdate", "2021-01-11")
+            return new Date (sessionStorage.getItem("startdate") + "T12:00:00")
         }
     }
 
@@ -23,18 +26,44 @@ function DateRangeSummary(){
             return new Date (localStorage.getItem("startdate"))
         }
         else {
-            return new Date ("2021-02-22")
+            sessionStorage.setItem("enddate", "2021-02-22")
+            return new Date (sessionStorage.getItem("enddate") + "T12:00:00")
         }
     }
 
-   /* const [selectedStartDate,setSelectedStartDate] = React.useState(
-        new Date(sessionStorage.getItem('startdate') + 'T12:00:00')
+    const [selectedStartDate,setSelectedStartDate] = React.useState(
+        getInitialStartDate()
     )
 
     const [selectedEndDate,setSelectedEndDate] = React.useState(
-        new Date(sessionStorage.getItem('enddate') + 'T12:00:00')
-    )*/
+        getInitialEndDate()
+    )
 
+    const handleStartDateChange= (date) =>{
+        setSelectedStartDate(date)
+        const data = { starttime: selectedStartDate };
+        var startDateArr = (selectedStartDate.toDateString()).split(" ");
+
+        var monthLetter = startDateArr[1];
+        var month = ProjectService.convertMonthToNumber(monthLetter);
+        var day = startDateArr[2];
+        var year = startDateArr[3];
+        var completeDate = year + "-" + month + "-" + day;
+        sessionStorage.setItem("startdate", completeDate);
+    }
+    const handleEndDateChange= (date) =>{
+        setSelectedEndDate(date)
+        const data2 = { endtime: selectedEndDate };
+        var endDateArr = (selectedEndDate.toDateString()).split(" ");
+
+        var monthLetter = endDateArr[1];
+        var month = ProjectService.convertMonthToNumber(monthLetter)
+        var day = endDateArr[2];
+        var year = endDateArr[3];
+
+        var completeDate = year + "-" + month + "-" + day;
+        sessionStorage.setItem("enddate", completeDate);
+    }
 
     return(
         <>
