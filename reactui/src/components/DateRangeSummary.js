63f7a20538ca74a2a7f255359795ee75443bbfe5
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-02 12:21:49 -0800

    set initial date

--- reactui/src/components/DateRangeSummary.js
+++ reactui/src/components/DateRangeSummary.js
@@ -1,6 +1,15 @@
 
 function DateRangeSummary(){
 
+    const [selectedStartDate,setSelectedStartDate] = React.useState(
+        new Date(sessionStorage.getItem('startdate') + 'T12:00:00')
+    )
+
+    const [selectedEndDate,setSelectedEndDate] = React.useState(
+        new Date(sessionStorage.getItem('enddate') + 'T12:00:00')
+    )
+
+
     return(
 
     )
