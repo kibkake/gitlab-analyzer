ec1e9187a9ff9ed590fc7e81160f24cd124d4440
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-04 12:59:13 -0800

    Refactor

--- reactui/src/components/SummaryScoreTable.js
+++ reactui/src/components/SummaryScoreTable.js
@@ -19,12 +19,12 @@ class SummaryScoreTable extends Component{
          }
      }
 
-    componentDidMount() {
+    async componentDidMount() {
         const {parentdata} = this.state;
-        this.getDataFromBackend(parentdata, this.props.startTime,  this.props.endTime)
+        await this.getDataFromBackend(parentdata, this.props.startTime,  this.props.endTime)
     }
 
-    getDataFromBackend (username, startTm, endTm) {
+    async getDataFromBackend (username, startTm, endTm) {
 
         const pathArray = window.location.pathname.split('/');
         const id = pathArray[2];
@@ -38,37 +38,27 @@ class SummaryScoreTable extends Component{
         }
 
         //request ref: http://localhost:8090/api/v1/projects/6/allTotalScores/user2/2021-01-01/2021-02-23
-        axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/" +
+        const response = await axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/" +
             startTm
             + "/" + endTm)
-            .then(response => {
-                const scores = response.data
-                this.setState({scoreSummary: scores})
-                console.log(this.state.scoreSummary);
-            }).catch((error) => {
-            console.error(error);
-        });
+
+        const scores = await response.data
+        await this.setState({scoreSummary: scores, parentdata: username,startTime: startTm,
+                    endTime: endTm})
+        //console.log(this.state.scoreSummary);
     }
 
-
     async componentDidUpdate(prevProps){
-        if(this.props.devName !== prevProps.devName){
-            await this.setState({parentdata: this.props.devName});
-            this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
-        }
-        if(this.props.startTime !== prevProps.startTime){
-            await this.setState({startTime: this.props.startTime});
-            this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
-        }
-        if(this.props.endTime !== prevProps.endTime){
-            await this.setState({endTime: this.props.endTime});
-            this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
+        if(this.props.devName !== prevProps.devName || this.props.startTime !== prevProps.startTime ||
+            this.props.endTime !== prevProps.endTime){
+            await this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
         }
     }
 
     render () {
         return (
             <div className="container">
+                {console.log("rendering")}
                 <Table striped bordered hover>
                         <tr>
                             <th>Commit</th>
