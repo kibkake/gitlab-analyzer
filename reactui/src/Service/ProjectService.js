88d20dc9e1df52626744027f387f8ebc0ab44b68
John Doknjas <jdoknjas@sfu.ca>
2021-03-08 13:11:43 -0800

    Shorten the convertMonthToNumber function.

--- reactui/src/Service/ProjectService.js
+++ reactui/src/Service/ProjectService.js
@@ -58,31 +58,10 @@ class ProjectService {
     }
 
     convertMonthToNumber(month) {
-        if(month === "Jan"){
-            return "01";
-        }else if(month === "Feb"){
-            return "02";
-        }else if(month === "Mar"){
-            return "03";
-        }else if(month === "Apr"){
-            return "04";
-        }else if(month === "May"){
-            return "05";
-        }else if(month === "Jun"){
-            return "06";
-        }else if(month === "Jul"){
-            return "07";
-        }else if(month === "Aug"){
-            return "08";
-        }else if(month === "Sep"){
-            return "09";
-        }else if(month === "Oct"){
-            return "10";
-        }else if(month === "Nov"){
-            return "11";
-        }else{
-            return "12"
-        }
+        const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const monthNum = monthList.indexOf(month) + 1;
+        return monthNum < 10 ? '0'.concat(monthNum.toString()) : monthNum.toString();
     }
 
     async getListOfDevs(repNum){
