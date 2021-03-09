263dbfa58758a24418574ba9167477e7f5d4acfd
Brandon Chan <brandon_chan@sfu.ca>
2021-03-08 21:45:22 -0800

    fix for merge

--- reactui/src/components/SummaryScoreTable.js
+++ reactui/src/components/SummaryScoreTable.js
@@ -13,39 +13,51 @@ class SummaryScoreTable extends Component{
          super(props);
          this.state = {
              scoreSummary:[],
-             parentdata: this.props.devName
+             parentdata: this.props.devName,
+             startTime: this.props.startTime,
+             endTime: this.props.endTime
          }
      }
 
-    componentDidMount() {
+     async componentDidMount() {
         const {parentdata} = this.state;
-        this.getDataFromBackend(parentdata)
-    }
+        await this.getDataFromBackend(parentdata, this.props.startTime,  this.props.endTime)
+     }
 
-    getDataFromBackend (username) {
+
+     async getDataFromBackend (username, startTm, endTm) {
 
         const pathArray = window.location.pathname.split('/');
         const id = pathArray[2];
+        var name = username;
+        if(sessionStorage.getItem('DeveloperNames' + id) != null && sessionStorage.getItem('Developers' + id) != null) {
+            for (var i = 0; i < JSON.parse(sessionStorage.getItem('Developers' + id)).length; i++) {
+                if (JSON.stringify(username) === JSON.stringify(JSON.parse(sessionStorage.getItem('Developers' + id))[i])) {
+                    name = JSON.parse(sessionStorage.getItem('DeveloperNames' + id))[i]//use name to retrieve data
+                    console.log("summary score table for: ", name)
+                }
+            }
+        }
 
-        //
         //request ref: http://localhost:8090/api/v1/projects/6/allTotalScores/user2/2021-01-01/2021-02-23
-        axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/2021-01-01/2021-02-23")
-            .then(response => {
-                const scores = response.data
-                this.setState({scoreSummary: scores})
-                console.log(this.state.scoreSummary);
-            }).catch((error) => {
-            console.error(error);
-        });
-    }
+        const response = await axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/" +
+            startTm + "/"
+            + endTm)
+
+        const scores = await response.data
+        await this.setState({scoreSummary: scores, parentdata: username,startTime: startTm,
+            endTime: endTm})
+     }
 
 
-    componentDidUpdate(prevProps){
-        if(this.props.devName !== prevProps.devName){
-            this.setState({parentdata: this.props.devName});
-            this.getDataFromBackend(this.props.devName)
+
+    async componentDidUpdate(prevProps){
+        if(this.props.devName !== prevProps.devName || this.props.startTime !== prevProps.startTime ||
+            this.props.endTime !== prevProps.endTime){
+            await this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
         }
     }
+
 
     render () {
         const {parentdata} = this.state;
