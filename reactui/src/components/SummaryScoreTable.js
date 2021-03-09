e278021423f85bd120d106b880f5467e2581384d
harveerk <harveer_khangura@sfu.ca>
2021-03-08 18:19:25 -0800

    comments

--- reactui/src/components/SummaryScoreTable.js
+++ reactui/src/components/SummaryScoreTable.js
@@ -13,55 +13,47 @@ class SummaryScoreTable extends Component{
          super(props);
          this.state = {
              scoreSummary:[],
-             parentdata: this.props.devName,
-             startTime: this.props.startTime,
-             endTime: this.props.endTime
+             parentdata: this.props.devName
          }
      }
 
-    async componentDidMount() {
+    componentDidMount() {
         const {parentdata} = this.state;
-        await this.getDataFromBackend(parentdata, this.props.startTime,  this.props.endTime)
+        this.getDataFromBackend(parentdata)
     }
 
-    async getDataFromBackend (username, startTm, endTm) {
+    getDataFromBackend (username) {
 
         const pathArray = window.location.pathname.split('/');
         const id = pathArray[2];
-        var name = username;
-        if(sessionStorage.getItem('DeveloperNames' + id) != null && sessionStorage.getItem('Developers' + id) != null) {
-            for (var i = 0; i < JSON.parse(sessionStorage.getItem('Developers' + id)).length; i++) {
-                if (JSON.stringify(username) === JSON.stringify(JSON.parse(sessionStorage.getItem('Developers' + id))[i])) {
-                    name = JSON.parse(sessionStorage.getItem('DeveloperNames' + id))[i]//use name to retrieve data
-                    console.log("summary score table for: ", name)
-                }
-            }
-        }
 
         //request ref: http://localhost:8090/api/v1/projects/6/allTotalScores/user2/2021-01-01/2021-02-23
-        const response = await axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/" +
-            startTm + "/"
-            + endTm)
-
-        const scores = await response.data
-        await this.setState({scoreSummary: scores, parentdata: username,startTime: startTm,
-                    endTime: endTm})
+        axios.get("/api/v1/projects/" + id + "/allTotalScores/"+ username +"/2021-01-01/2021-02-23")
+            .then(response => {
+                const scores = response.data
+                this.setState({scoreSummary: scores})
+                console.log(this.state.scoreSummary);
+            }).catch((error) => {
+            console.error(error);
+        });
     }
 
-    async componentDidUpdate(prevProps){
-        if(this.props.devName !== prevProps.devName || this.props.startTime !== prevProps.startTime ||
-            this.props.endTime !== prevProps.endTime){
-            await this.getDataFromBackend(this.props.devName, this.props.startTime,this.props.endTime)
+
+    componentDidUpdate(prevProps){
+        if(this.props.devName !== prevProps.devName){
+            this.setState({parentdata: this.props.devName});
+            this.getDataFromBackend(this.props.devName)
         }
     }
 
     render () {
         const {parentdata} = this.state;
+        // copy button
         let toCopy = "Commits: " + this.state.scoreSummary.totalCommitScore + " Merge Requests: " + this.state.scoreSummary.totalMergeRequestScore + " Word count of comments: " + this.state.scoreSummary.totalCommentWordCount;
         return (
             <div className="container">
                 <Table striped bordered hover>
-                    <tr>
+                        <tr>
                             <th>Commit</th>
                             <th>Merge Request</th>
                             <th>Word Count of Comments</th>
