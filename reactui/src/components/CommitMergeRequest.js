5d7d37fb902c01e11837a6a24912730dbb188bdb
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-11 01:10:34 -0800

    Check if there is an MR to display

--- reactui/src/components/CommitMergeRequest.js
+++ reactui/src/components/CommitMergeRequest.js
@@ -1,32 +1,19 @@
 import React, {PureComponent} from "react";
 import "./HBox.css"
-
+import axios from "axios";
 
 class CommitMergeRequest extends PureComponent {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
+            data: {mrScore: 0, diffs : [{diff: []}]},
             hash : this.props.hash
         }
     }
 //http://localhost:8090/api/v2/projects/6/mergeRequests/81dcd6aab70ebf99195e234d9f4f49ec13d0a252
     async componentDidMount() {
-        /*var str = window.location.pathname;
-        var repNum = str.split("/")[2];
-        var hash = this.props.hash;
-
-        let url2 = '/api/v2/projects/' + repNum + '/mergeRequests/' + hash;
-        const result = await fetch(url2, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        const resp = await result.json();
-        await this.setState({data:resp})*/
+        await this.getDataFromBackend();
     }
 
     async getDataFromBackend () {
@@ -47,6 +34,14 @@ class CommitMergeRequest extends PureComponent {
     }
 
     render() {
+
+        if(this.state.data.diff.length === 0){
+            return(
+                <div>
+                    no merge request!
+                    </div>
+            )
+        }
 
         return(
         <div style={{ overflow: "scroll", height: "1050px", width: "300px"}}>
