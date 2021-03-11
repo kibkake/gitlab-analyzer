11735f761d6c5482d4dad78a259c252f9f28a7b2
Brandon Chan <brandon_chan@sfu.ca>
2021-03-10 18:09:07 -0800

    applied changes to login token component and fixed default submit bug.

--- reactui/src/components/LoginToken.js
+++ reactui/src/components/LoginToken.js
@@ -6,13 +6,6 @@ import ProfileService from "../Service/ProfileService";
 export default function LoginToken(){
 
     const [usertoken, setUserToken] = useState("");
-    const [token_toggle, setTokenToggle] = useState(false);
-
-    // event handler onSubmit
-    const toggleTokenSubmit = event => {
-        setTokenToggle(!token_toggle);
-        event.preventDefault();
-    }
 
     // event handler onSubmit
     const handleSubmit = async event => {
@@ -22,28 +15,28 @@ export default function LoginToken(){
             sessionStorage.setItem("user","temp");
             sessionStorage.setItem("token",usertoken);
             await ProfileService.changeUserToken("temp",usertoken);
-            toggleTokenSubmit(event);
             window.location.href="/Home";
         }
         event.preventDefault();
     }
 
+    // cancel
+    const handleCancel = event =>{
+        window.location.reload();
+        event.preventDefault();
+    }
+
     return(
         <>
-            {!token_toggle &&
-                <button className="login" onClick={toggleTokenSubmit}><SiJsonwebtokens/> Login with Token </button>
-            }
-            { token_toggle &&
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <h5>Token:</h5>
-                        <input name="username" type="text"  onChange={event => setUserToken(event.target.value)} />
-                    </label>
-                    <br/>
-                    <br/>
-                    <button className="login" type="submit" ><SiJsonwebtokens/> Login with Token</button> <div className="spacer"/> <button className="cancel" onClick={toggleTokenSubmit}><TiCancel/> Cancel</button>
-                </form>
-            }
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <h5>Token:</h5>
+                    <input name="username" type="text"  onChange={event => setUserToken(event.target.value)} />
+                </label>
+                <br/>
+                <br/>
+                <button className="login" type="submit" ><SiJsonwebtokens/> Login with Token</button> <div className="spacer"/> <button className="cancel" onClick={handleCancel}><TiCancel/> Cancel</button>
+            </form>
         </>
     );
 }
