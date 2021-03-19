76bbab047f82a78716f5cbcd2d6f0942f8aa2d3d
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-18 19:34:25 -0700

    Get and format date

--- reactui/src/components/DropDownMenuSummary.js
+++ reactui/src/components/DropDownMenuSummary.js
@@ -5,6 +5,7 @@ import Navbar_Developers from "./NavBars_Menu/Navbar_Developers";
 import './DropDownMenu.css';
 import DateRangeSummary from './DateRangeSummary'
 import "./HBox.css"
+import ProjectService from "../Service/ProjectService";
 
 
 
@@ -29,11 +30,55 @@ function DropDownMenuSummary ({listOfDevelopers}) {
         sessionStorage.setItem("CurrentDeveloper", obj.label)
     }
 
+    function getInitialStartDate() {
+
+        if(sessionStorage.getItem("startdate") != null){
+            return new Date (sessionStorage.getItem("startdate") + "T12:00:00")
+        }
+        else if(localStorage.getItem("startdate") != null){
+            sessionStorage.setItem("startdate", localStorage.getItem("startdate"))
+            return new Date (localStorage.getItem("startdate") + "T12:00:00")
+        }
+        else {
+            sessionStorage.setItem("startdate", "2021-01-11")
+            return new Date (sessionStorage.getItem("startdate") + "T12:00:00")
+        }
+    }
+
+    function getInitialEndDate() {
+
+        if(sessionStorage.getItem("enddate") != null){
+            return new Date (sessionStorage.getItem("enddate") + "T12:00:00")
+        }
+        else if(localStorage.getItem("enddate") != null){
+            sessionStorage.setItem("enddate", localStorage.getItem("enddate"))
+            return new Date (localStorage.getItem("enddate") + "T12:00:00")
+        }
+        else {
+            sessionStorage.setItem("enddate", "2021-02-22")
+            return new Date (sessionStorage.getItem("enddate") + "T12:00:00")
+        }
+    }
+
+    function changeDateFormat(time) {
+
+        console.log("changing format", time)
+
+        var startDateArr = (time.toDateString()).split(" ");
+
+        var monthLetter = startDateArr[1];
+        var month = ProjectService.convertMonthToNumber(monthLetter);
+        var day = startDateArr[2];
+        var year = startDateArr[3];
+        var completeDate = year + "-" + month + "-" + day;
+        return completeDate;
+    }
+
     return (
         <div>
             <Navbar_Developers devName = {selectedValue}/>
 
-            <div style="DevMenu">
+            <div>
                 <div className="DropDownMenu">
                 <Select
                     options={devArray}
