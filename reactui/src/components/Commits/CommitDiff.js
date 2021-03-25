e29b087618e8254aa882f7459dc3e64bcb8a09c4
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-25 14:51:46 -0700

    Get data from back end. Add handler

--- reactui/src/components/Commits/CommitDiff.js
+++ reactui/src/components/Commits/CommitDiff.js
@@ -12,6 +12,7 @@ import Row from "./RowsCodeDiff";
 import Typography from "@material-ui/core/Typography";
 import moment from "moment";
 import ExpandButton from "./ExpandButton";
+import Spinner from "react-bootstrap/Spinner";
 
 class SingleCommitDiff extends Component{
     constructor(props){
@@ -21,18 +22,52 @@ class SingleCommitDiff extends Component{
             hash : this.props.hash,
             expanded: false
         };
+        this.handler = this.handler.bind(this)
 
     }
 
+    async handler() {
 
+        if(this.state.expanded === true) {
+            await this.setState({
+                expanded: false
+            })
+        } else{
+            await this.setState({
+                expanded: true
+            })
+        }
+    }
+
+    async componentDidMount() {
+        await this.getDataFromBackend();
+    }
+
+    async getDataFromBackend(){
+        var str = window.location.pathname;
+        var repNum = str.split("/")[2];
+        var hash = this.props.hash;
+
+        let url2 = '/api/v1/projects/' + repNum + '/Commit/' + hash;
+        const result = await fetch(url2, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        const resp = await result.json();
+        await this.setState({data:resp})
+    }
 
     render() {
+
         return (
             <div>
+            <ExpandButton ever = {this.state.data} handler = {this.handler} expanded = {this.state.expanded}></ExpandButton>
                 </div>
         )
     }
 }
 
 export default SingleCommitDiff;
-
