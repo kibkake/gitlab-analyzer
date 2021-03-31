bbd95d1cef968c1025b4804d1ee38f94534428bc
nsimms <nsimms@sfu.ca>
2021-03-30 18:45:09 -0700

    remove calls related to removed calls in the mr controller

--- Java/src/main/java/DatabaseClasses/Service/MergeRequestService.java
+++ Java/src/main/java/DatabaseClasses/Service/MergeRequestService.java
@@ -27,30 +27,6 @@ public class MergeRequestService {
         mergeRequestRepository.saveAll(new MergeRequestConnection().getProjectMergeRequestsFromGitLab(projectId));
     }
 
-    public List<MergeRequestDateScore> getMrScorePerDay(int projectId, String userName, LocalDate startDate, LocalDate endDate) {
-        return mergeRequestRepository.getDevsMrsScoreADay(projectId, userName, startDate, endDate);
-    }
-
-    public MergeRequest getMergeRequest(int projectId, int mrId) {
-        return mergeRequestRepository.findByProjectIdAndId(projectId, mrId);
-    }
-
-    public List<MergeRequest> getProjectMRs(int projectId) {
-        return mergeRequestRepository.findByProjectId(projectId);
-    }
-
-    public List<MergeRequest> getUserMergeRequests(int projectId, String authorName, LocalDate startLocalTime,
-                                                   LocalDate endLocalTime) {
-        return mergeRequestRepository.getDevMergeRequests(projectId, authorName,
-                startLocalTime, endLocalTime);
-    }
-
-    public Object getTotalMergeRequestScore(int projectId, String authorName, LocalDate startLocalTime,
-                                                  LocalDate endLocalTime) {
-        return mergeRequestRepository.getUserTotalMergeRequestScore(projectId, authorName,
-                startLocalTime, endLocalTime);
-    }
-
     public MergeRequest getMrByCommitHash(int projectId, String hash) {
         return mergeRequestRepository.getMrByCommitHash(projectId, hash);
 
