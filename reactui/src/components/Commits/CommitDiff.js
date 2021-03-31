2b327e4b776471e8e665d8a4e3c69656583851ef
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-31 01:17:32 -0700

    Display loading message until data is loaded

--- reactui/src/components/Commits/CommitDiff.js
+++ reactui/src/components/Commits/CommitDiff.js
@@ -6,12 +6,6 @@ import TableContainer from "@material-ui/core/TableContainer";
 import Paper from "@material-ui/core/Paper";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import TableBody from "@material-ui/core/TableBody";
-import Row from "./RowsCodeDiff";
-import Typography from "@material-ui/core/Typography";
-import moment from "moment";
-import ExpandButton from "./ExpandButton";
 import Spinner from "react-bootstrap/Spinner";
 
 class SingleCommitDiff extends Component{
@@ -20,14 +14,13 @@ class SingleCommitDiff extends Component{
         this.state={
             data: [],
             hash : this.props.hash,
-            expanded: false
+            expanded: false,
+            commits : this.props.commits
         };
         this.handler = this.handler.bind(this)
-
     }
 
     async handler() {
-
         if(this.state.expanded === true) {
             await this.setState({
                 expanded: false
@@ -58,14 +51,36 @@ class SingleCommitDiff extends Component{
         })
         const resp = await result.json();
         await this.setState({data:resp})
+        console.log(resp)
+    }
+
+    async componentDidUpdate(prevProps){
+        if(this.props.hash !== prevProps.hash){
+            await this.getDataFromBackend()
+        }
     }
 
     render() {
-
+        if(this.state.data.length === 0){
+            return (
+                <div
+                    style={{ overflow: "scroll", minHeight: "1000px", width: "1000px"}}>
+                    <br>
+                    </br>
+                    <text style={{fontWeight: 'bold', fontSize: '20px', color: 'blue'}}>
+                        Loading commit diff....
+                    </text>
+                    <Spinner
+                        animation="border"
+                        variant="primary"
+                        size="la"
+                    />
+                </div>
+            )
+        }
         return (
             <div>
-            <ExpandButton ever = {this.state.data} handler = {this.handler} expanded = {this.state.expanded}></ExpandButton>
-                </div>
+            </div>
         )
     }
 }
