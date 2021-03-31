7afc8418090dd867ae0e2020e66d9c1f69c543ee
arahilin <abtin_rahili_nejad@sfu.ca>
2021-03-31 02:22:00 -0700

    Get data passed in instead of fetching from back end.

--- reactui/src/components/Commits/CommitsPerDay.js
+++ reactui/src/components/Commits/CommitsPerDay.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
 import Button from 'react-bootstrap/Button';
 import  "./HBox.css"
+import "../Projects/ProjectList.css";
+import moment from "moment";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import '../MergeRequest/MergeListTable.css'
+
 
 class CommitsPerDay extends Component{
     constructor(props){
@@ -8,8 +14,14 @@ class CommitsPerDay extends Component{
         this.state={
             data: [],
             devName: this.props.devName,
-            startTime: this.props.startTime
+            startTime: this.props.startTime,
+            date : ""
         };
+        this.handler = this.handler.bind(this)
+    }
+
+    async handler(hash) {
+        this.props.handler(hash)
     }
 
     async componentDidMount(){
@@ -17,7 +29,7 @@ class CommitsPerDay extends Component{
     }
 
     async getDataFromBackend(userName, date) {
-        var str = window.location.pathname;
+        /*var str = window.location.pathname;
         var repNum = str.split("/")[2];
 
         let url2 = '/api/v1/projects/' + repNum + '/Commits/' + userName + '/' + date + "/" + date  + "/either"
@@ -29,45 +41,55 @@ class CommitsPerDay extends Component{
             }
         })
         const resp = await result.json();
-        await this.setState({data:resp})
+        await this.setState({data:resp})*/
+
+       // console.log(date)//2021-02-06
+
+
+        var listOfDates = [];
+        var tempDate = "";
+        this.props.commits.map(function(item) {
+            var time = moment(item.committed_date).format('L')//02/18/2021
+            tempDate = moment(date).format('lll')
+            const month = time.substring(0,2)
+            const day = time.substring(3,5)
+            const year = time.substring(6,10)
+
+            const fulltime = year + '-' + month + '-' + day
+            if(fulltime === date){
+                listOfDates.push(item)
+            }
+        })
+        await this.setState({data:listOfDates, date: tempDate})
     }
 
     async componentDidUpdate(prevProps){
-        console.log(this.props.devName)
-        if(this.props.devName !== prevProps.devName ||
-            this.props.startTime !== prevProps.startTime){
+        if(this.props.startTime !== prevProps.startTime){
             await this.getDataFromBackend(this.props.devName, this.props.startTime)
         }
     }
 
     render(){
-
-        var output = this.state.data.map(function(item) {
+        var output = []
+        output = this.state.data.map(function(item) {
             return {
                 id: item.id,
                 date: item.committed_date
             };
         });
-        //console.log(output)
+        console.log(output)
 
         return(
-            <ul style={{ overflow: "scroll", height: "1050px", width: "1000px"}}>
-            {this.state.data.map(item => {
-                return <li >
-                    <a href= {"Developers/" + item }target= "_blank">
-                        <Button className="Footer2"to={item.id}
-                                type="button"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    {this.props.handler(item.id)}
+            <TableContainer
+                style={{ overflowX: "scroll" , height: "1050px", width: "500px"}}
+                component={Paper}
+                display="flex"
+                flexDirection="row"
+                p={1}
+                m={1}
+                justifyContent="flex-start">
 
-                                }}>
-                            <span >{item.id}          commited at:      {item.committed_date.substring(11,19)}</span>
-                        </Button>
-                    </a>
-                </li>;
-            })}
-        </ul>
+            </TableContainer>
         );
     }
 }
