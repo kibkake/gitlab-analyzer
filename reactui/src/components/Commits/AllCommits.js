7b717567f3c3cc6ebea616727f85569182e7c4bb
arahilin <abtin_rahili_nejad@sfu.ca>
2021-04-05 01:52:31 -0700

    Clean up

--- reactui/src/components/Commits/AllCommits.js
+++ reactui/src/components/Commits/AllCommits.js
@@ -28,26 +28,6 @@ class AllCommits extends Component{
         this.props.handler(hash)
     }
 
-    async componentDidMount(){
-    }
-
-    async getDataFromBackend(userName, startDate, endDate) {
-        var str = window.location.pathname;
-        var repNum = str.split("/")[2];
-
-        let url2 = '/api/v1/projects/' + repNum + '/Commits/' + userName + '/' + startDate + "/" + endDate  + "/either"
-        const result = await fetch(url2, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        const resp = await result.json();
-        await this.setState({data:resp})
-        await this.props.handler3(this.state.data)
-    }
-
     render(){
 
         var output = this.state.data.map(function(item) {
