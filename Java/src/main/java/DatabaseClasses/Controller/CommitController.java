454ac05891aea6eb2abc3a2be80841a8af8f4067
nsimms <nsimms@sfu.ca>
2021-04-10 00:37:45 -0700

    git rid of unneeded calls in CommitController

--- Java/src/main/java/DatabaseClasses/Controller/CommitController.java
+++ Java/src/main/java/DatabaseClasses/Controller/CommitController.java
@@ -31,50 +31,6 @@ public class CommitController {
         commitService.saveProjectCommits(projectId);
     }
 
-    @GetMapping("projects/{projectId}/Commits/{authorName}/{start}/{end}")
-    public List<Commit> getAllUserCommits(@PathVariable("projectId") int projectId,
-                                          @PathVariable("authorName") String authorName,
-                                          @PathVariable("start") String start,
-                                          @PathVariable("end")String end) {
-
-
-        OffsetDateTime startDateWithOffSet = OffsetDateTime.parse(start + isoEnding);
-        OffsetDateTime endDateWithOffSet = OffsetDateTime.parse(end + isoEnding);
-        Date startDate = Date.from(startDateWithOffSet.toInstant());
-        Date endDate = Date.from(endDateWithOffSet.toInstant());
-        return commitService.getUserCommits(projectId, authorName, startDate, endDate);
-    }
-
-    @GetMapping("projects/{projectId}/Commits/scores/total/{userName}/{startDate}/{endDate}")
-    public Object getTotalCommitScores(@PathVariable int projectId, @PathVariable String userName,
-                                                 @PathVariable String startDate, @PathVariable String endDate) {
-        LocalDate StartLocalTime = LocalDate.parse(startDate);
-        LocalDate endLocalTime = LocalDate.parse(endDate);
-        return commitService.getTotalCommitScore(projectId, userName, StartLocalTime, endLocalTime);
-    }
-
-    @GetMapping("projects/{projectId}/Commits/scores/{committerName}/{start}/{end}")
-    public List<CommitDateScore> getUserCommitScoresWithDates(@PathVariable("projectId") int projectId,
-                                                              @PathVariable("committerName") String committerName,
-                                                              @PathVariable("start") String start,
-                                                              @PathVariable("end")String end) {
-
-        LocalDate StartLocalTime = LocalDate.parse(start);
-        LocalDate endLocalTime = LocalDate.parse(end);
-        return commitService.getScorePerDay(projectId, committerName, StartLocalTime, endLocalTime);
-    }
-
-    @GetMapping("projects/{projectId}/Commits/array/{committerName}/{start}/{end}")
-    public List<CommitDateScore> getCommitsArray(@PathVariable("projectId") int projectId,
-                                           @PathVariable("committerName") String committerName,
-                                           @PathVariable("start") String start,
-                                           @PathVariable("end")String end) {
-        LocalDate StartLocalTime = LocalDate.parse(start);
-        LocalDate endLocalTime = LocalDate.parse(end);
-        return commitService.getUserCommitsArray(projectId, committerName, StartLocalTime, endLocalTime);
-    }
-
-
     @GetMapping("projects/{projectId}/Commits/{hash}")
     public Commit getCommit(@PathVariable("projectId") int projectId,
                             @PathVariable("hash") String hash) {
